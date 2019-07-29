19416cd09d12579e685c153b82621e9b2ce91294
Daniel Borowski <danielborowski@Daniels-MacBook-Pro-5.local>
2019-07-29 14:03:19 -0400

    remove s

--- StitchApp/functions/getDBCollection/source.js
+++
@@ -1,8 +0,0 @@
-exports = function(arg){
-  const db_name = context.values.get("db_name");
-  const coll_name = context.values.get("coll_name");
-  return {
-    'db_name': db_name,
-    'coll_name': coll_name
-  };
-};
--- StitchApp/functions/validateNewJob/source.js
+++
@@ -1,6 +0,0 @@
-exports = function(payload){
-  // do some kind of validation'
-  
-  return {valid: true};
-  return {valid: false, error: "The payload is invalid"};
-};
