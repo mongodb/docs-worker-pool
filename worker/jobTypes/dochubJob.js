790d923675234e0a20c69d9ec295d650a7017adc
Varsha Subrahmanyam <varsha.subrahmanyam@Varshas-MacBook-Pro-2.local>
2019-11-12 16:12:31 -0500

    job is now picked up

--- worker/jobTypes/dochubJob.js
+++ worker/jobTypes/dochubJob.js
@@ -1,24 +1,11 @@
-/**
- *  this is the triggered processor for running 
- *  Dochub jobs.
- *  an example job definition lives in jobDef.json
- */
-
 const fs = require('fs-extra');
 const workerUtils = require('../utils/utils');
+const DochubJob = require('../jobTypes/dochubJob').DochubJobClass;
+const validator = require('validator');
 
 const buildTimeout = 60 * 450;
 
 const invalidJobDef = new Error('job not valid');
-
-// get base path for public/private repos
-function getBasePath(currentJob) {
-  let basePath = `https://github.com`;
-  if (currentJob.payload.private) {
-    basePath = `https://${process.env.GITHUB_BOT_USERNAME}:${process.env.GITHUB_BOT_PASSWORD}@github.com`;
-  }
-  return basePath;
-}
 
 //anything that is passed to an exec must be validated or sanitized
 //we use the term sanitize here lightly -- in this instance this // ////validates
@@ -52,154 +39,8 @@ function safeDochub(currentJob) {
   throw invalidJobDef;
 }
 
-async function build(currentJob) {
-  workerUtils.logInMongo(
-    currentJob,
-    `${'    (BUILD)'.padEnd(15)}Running Build`
-  );
-  // Perform the build --> exec is weird
-  try {
-    const exec = workerUtils.getExecPromise();
-    workerUtils.logInMongo(
-      currentJob,
-      `${'    (BUILD)'.padEnd(15)}running worker.sh`
-    );
-
-    const basePath = getBasePath(currentJob);
-    const repoPath = basePath + '/' + currentJob.payload.repoOwner + '/' + currentJob.payload.repoName;
-    
-    if (currentJob.payload.branchName != 'master') {
-      const command = `cd repos/${currentJob.payload.repoName}; git checkout ${
-        currentJob.payload.branchName
-        }; git pull origin ${currentJob.payload.branchName};`;
-      
-      await exec(command);
-   
-      const commandbuild = `. /venv/bin/activate; cd repos/${currentJob.payload.repoName}; chmod 755 worker.sh; ./worker.sh`;
-      const execTwo = workerUtils.getExecPromise();
-
-      const { stdout, stderr } = await execTwo(commandbuild);
-
-      workerUtils.logInMongo(
-        currentJob,
-        `${'    (BUILD)'.padEnd(15)}ran worker.sh`
-      );
-
-      workerUtils.logInMongo(
-        currentJob,
-        `${'    (BUILD)'.padEnd(15)}worker.sh run details:\n\n${stdout}\n---\n${stderr}`
-      );
-
-      // only post entire build output to slack if there are warnings
-      const buildOutputToSlack = stdout + '\n\n' + stderr;
-
-      if (buildOutputToSlack.indexOf('WARNING:') !== -1) {
-        workerUtils.populateCommunicationMessageInMongo(currentJob, buildOutputToSlack);
-      }
-      
-    } else {
-      workerUtils.logInMongo(
-        currentJob,
-        `${'    (BUILD)'.padEnd(15)} failed, master branch not supported`
-      );
-      throw new Error('master branches not supported');
-    }
-
-  } catch (errResult) {
-    console.log('error ' + errResult);
-    if (
-      errResult.hasOwnProperty('code') ||
-      errResult.hasOwnProperty('signal') ||
-      errResult.hasOwnProperty('killed')
-    ) {
-      workerUtils.logInMongo(
-        currentJob,
-        `${'    (BUILD)'.padEnd(15)}failed with code: ${errResult.code}`
-      );
-      workerUtils.logInMongo(
-        currentJob,
-        `${'    (BUILD)'.padEnd(15)}stdErr: ${errResult.stderr}`
-      );
-      throw errResult;
-    }
-  }
-  console.log('finished build');
-  workerUtils.logInMongo(
-    currentJob,
-    `${'    (BUILD)'.padEnd(15)}Finished Build`
-  );
-}
-
-async function addToFastly(currentJob) {
-  workerUtils.logInMongo(
-    currentJob,
-    `${'    (GIT)'.padEnd(15)}Cloning repository`
-  );
-  if (!currentJob.payload.branchName) {
-    workerUtils.logInMongo(
-      currentJob,
-      `${'    (CLONE)'.padEnd(15)}failed due to insufficient definition`
-    );
-    throw new Error('branch name not indicated');
-  }
-  // clone the repo we need to build
-  try {
-
-    const basePath = getBasePath(currentJob);
-    const repoPath = basePath + '/' + currentJob.payload.repoOwner + '/' + currentJob.payload.repoName;
-
-    await simpleGit('repos')
-      .silent(false)
-      .clone(repoPath)
-      .catch(err => {
-        console.error('failed: ', err);
-        throw err;
-      });
-    workerUtils.logInMongo(currentJob, `${'    (GIT)'.padEnd(15)}ran fetch`);
-  } catch (errResult) {
-    if (
-      errResult.hasOwnProperty('code') ||
-      errResult.hasOwnProperty('signal') ||
-      errResult.hasOwnProperty('killed')
-    ) {
-      workerUtils.logInMongo(
-        currentJob,
-        `${'    (GIT)'.padEnd(15)}failed with code: ${errResult.code}`
-      );
-      workerUtils.logInMongo(
-        currentJob,
-        `${'    (GIT)'.padEnd(15)}stdErr: ${errResult.stderr}`
-      );
-      // console.log('\n\nstdout:', errResult.stdout);
-      throw errResult;
-    }
-  }
-
-  workerUtils.logInMongo(
-    currentJob,
-    `${'    (GIT)'.padEnd(15)}Finished git clone`
-  );
-}
-
-
-async function cleanup(currentJob) {
-  workerUtils.logInMongo(
-    currentJob,
-    `${'    (rm)'.padEnd(15)}Cleaning up repository`
-  );
-  try {
-    workerUtils.removeDirectory(`repos/${currentJob.payload.repoName}`);
-    workerUtils.logInMongo(
-      currentJob,
-      `${'    (rm)'.padEnd(15)}Finished cleaning repo`
-    );
-  } catch (errResult) {
-    throw errResult;
-  }
-}
-
 async function runDochub(currentJob) {
-  workerUtils.logInMongo(currentJob, ' ** Adding source/target links to Fastly edge dictionary');
+  workerUtils.logInMongo(currentJob, ' ** Running dochub-fastly migration');
 
   if (
     !currentJob ||
@@ -207,47 +48,39 @@ async function runDochub(currentJob) {
     !currentJob.payload.source ||
     !currentJob.payload.target
   ) {
-    workerUtils.logInMongo(
-      currentJob,
-      `${'    (BUILD)'.padEnd(15)}failed due to insufficient definition`
-    );
+    workerUtils.logInMongo(currentJob,`${'(BUILD)'.padEnd(15)}failed due to insufficient definition`);
     throw invalidJobDef;
   }
 
-  // execute the build
-  await workerUtils.promiseTimeoutS(
-    buildTimeout,
-    cleanup(currentJob),
-    'Timed out on rm'
-  );
-
-  // execute the build
-  await workerUtils.promiseTimeoutS(
-    buildTimeout,
-    build(currentJob),
-    'Timed out on build'
-  );
+  // instantiate dochub job class
+  const job = new DochubJob(currentJob);
+  const publisher = new S3Publish(job);
 
   console.log('completed build');
 
-  console.log('adding source and target to Fastly');
-  await workerUtils.promiseTimeoutS(
-    buildTimeout,
-    addToFastly(currentJob),
-    'Timed out on add to Fastly'
-  );
+  let branchext = '';
+  let isMaster = true;
+
+  if (currentJob.payload.branchName !== 'master') {
+    branchext = '-' + currentJob.payload.branchName;
+    isMaster = false;
+  }
+
+  if (isMaster) {
+    // TODO: push to prod
+  } else {
+    console.log('pushing to stage');
+    await pushToStage(publisher, logger);
+  }
 
   const files = workerUtils.getFilesInDir(
     './' + currentJob.payload.repoName + '/build/public' + branchext
   );
 
-  return files; // what do i return??
+  return files;
 }
 
 module.exports = {
   runDochub,
-  cleanup,
-  addToFastly,
-  build,
   safeDochub,
 };
