af0ae038a1e84c5c01e21a51d6a512a2587fe0d2
Varsha Subrahmanyam <varsha.subrahmanyam@Varshas-MacBook-Pro-2.local>
2019-11-14 13:49:13 -0500

    can connect to dochub database; fastly is working

--- worker/jobTypes/publishDochubJob.js
+++ worker/jobTypes/publishDochubJob.js
@@ -54,14 +54,80 @@ async function runPublishDochub(currentJob) {
   // instantiate dochub job class
   // const job = new DochubJob(currentJob);
 
-  // add source and target to Fastly edge dictionary
+    // add source and target to Fastly edge dictionary
   var fastly = require('fastly')('6aRkvo3EJN7N2JLJcZdOaS7AxFKMu6qq') // put this token elsewhere!
 
+  const options = {
+    name: "redirect_map"
+  }
   // fastly.request('GET', '/content/edge_check?url=docs.mongodb.com', function (err, obj) {
-  fastly.request('GET', '/service/0U4FLNfta0jDgmrSFA193k/version/35/dictionary/redirect_map', function (err, obj) {
+  fastly.request('GET', '/service/0U4FLNfta0jDgmrSFA193k/version/36/dictionary', options, function (err, obj) {
     if (err) return console.dir(err);
     console.dir(obj);
   });
+
+  var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+  var request = new XMLHttpRequest();
+
+  // connect to MongoDB dochub database
+  const MongoClient = require("mongodb").MongoClient;
+  assert = require("assert")
+
+  // connection url - PUT USERNAME/PASSWORD ELSEWHERE
+  const url = 'mongodb+srv://varsha:gypsh3K0mL55Nl7w@cluster0-ylwlz.mongodb.net/dochub?authSource=admin';
+
+  MongoClient.connect(url, function(err, client) {
+    assert.equal(null, err);
+
+    const db = client.db("dochub");
+
+    var cursor = db.collection('keys').find({});
+
+    function iterateFunc(doc) {
+      page = "https://dochub.mongodb.org/core/" + doc.name;
+      console.log(JSON.stringify(doc, null, 4));
+    }
+
+    function errorFunc(error) {
+      console.log(error);
+    }
+
+    cursor.forEach(iterateFunc, errorFunc);
+
+
+    client.close();
+  });
+
+
+  var page = "http://www.googlevaralsdj.com";
+  request.open("GET", page, false);
+  request.send()
+  if (request.status === 200) {
+    console.log("real page: ", page)
+  } else {
+    console.log("fake page: ", page)
+  }
+
+  const new_options = {
+    item_key: page,
+    item_value: "Fake Google"
+  }
+
+  // dictionary_id: 5dNt5O5Kr6cD6eIbkexmbb
+  fastly.request('POST', '/service/0U4FLNfta0jDgmrSFA193k/dictionary/5dNt5O5Kr6cD6eIbkexmbb/item', new_options, function (err, obj) {
+    if (err) return console.dir(err);
+    console.dir(obj);
+  });
+
+  fastly.request('GET', '/service/0U4FLNfta0jDgmrSFA193k/version/36/dictionary', options, function (err, obj) {
+    if (err) return console.dir(err);
+    console.dir(obj);
+  });
+
+  // fastly.request('GET', '/service/0U4FLNfta0jDgmrSFA193k/version/36/dictionary/redirect_map', function (err, obj) {
+  //   if (err) return console.dir(err);
+  //   console.dir(obj);
+  // });
 }
 
 module.exports = {
