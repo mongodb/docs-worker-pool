5c77a96d86fcb493391fdbe38b3e72079c5eb24e
Daniel Borowski <danielborowski@Daniels-MacBook-Pro-5.local>
2019-12-09 16:53:26 -0500

    deploy init

--- StitchApp/WorkerPoolStaging/functions/getAdmins/source.js
+++
@@ -1,8 +0,0 @@
-exports = function(arg) {
-  return [
-    'sue.kerschbaumer@10gen.com',
-    //'daniel.borowski@10gen.com',
-    //'elle.shwer@mongodb.com',
-    'maddie.zechar@mongodb.com',
-  ];
-};
