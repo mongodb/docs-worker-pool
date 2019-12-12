6bc5e2cb25b0eaf21dde8ac35155403ffc6f75e2
madelinezec <mez2113@columbia.edu>
2019-12-12 18:21:36 -0500

    map production deploy job in worker.js

--- worker/worker.js
+++ worker/worker.js
@@ -8,7 +8,7 @@ const workerUtils = require('./utils/utils');
 // **** IF YOU ARE ADDING A FUNCTION --> IMPORT IT HERE
 // Import job function
 const { runGithubPush, safeGithubPush } = require('./jobTypes/githubPushJob');
-
+const {runGithubProdPush, safeGithubProdPush} = require('./jobTypes/productionDeployJob')
 // Variables
 let queueCollection; // Holder for the queueCollection in MongoDB Atlas
 let metaCollection; // Holder for the meta collection which holds repos/branches to build
@@ -33,6 +33,7 @@ const maxCheckIn = (2 * MONGO_TIMEOUT_S + JOB_TIMEOUT_S + 60 * 10) * 1000;
 // Dictionary of possible jobs for this node
 const jobTypeToFunc = {
   githubPush: { function: runGithubPush, safe: safeGithubPush },
+  productionDeploy : {function: runGithubProdPush, safe: safeGithubProdPush},
 };
 
 // route for liveness check
