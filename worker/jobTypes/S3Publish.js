8dd32df99a9896a72fa2e16afb133cc14e26065d
madelinezec <mez2113@columbia.edu>
2020-02-12 11:49:10 -0500

    change error clause

--- worker/jobTypes/S3Publish.js
+++ worker/jobTypes/S3Publish.js
@@ -69,16 +69,16 @@ class S3PublishClass {
       `. /venv/bin/activate`,
       `cd repos/${this.GitHubJob.getRepoDirName()}`,
       `make publish`,
-      `make stage`
+      `make deploy`
     ];
 
     // check if need to build next-gen
     if (this.GitHubJob.buildNextGen()) {
       deployCommands[deployCommands.length - 2] = 'make next-gen-publish';
-      deployCommands[deployCommands.length - 1] = 'make next-gen-stage';
+      deployCommands[deployCommands.length - 1] = 'make next-gen-deploy';
     }
 
-    logger.save(`${'(stage)'.padEnd(15)}Pushing to staging`);
+    logger.save(`${'(stage)'.padEnd(15)}Pushing to production`);
 
     try {
       const exec = workerUtils.getExecPromise();
@@ -90,9 +90,9 @@ class S3PublishClass {
         stdoutMod = stdout.substr(stdout.indexOf('Summary'));
       }
       return new Promise(function(resolve, reject) {
-        logger.save(`${'(stage)'.padEnd(15)}Finished pushing to staging`);
+        logger.save(`${'(prod)'.padEnd(15)}Finished pushing to production`);
         logger.save(
-          `${'(stage)'.padEnd(15)}Staging push details:\n\n${stdoutMod}`
+          `${'(prod)'.padEnd(15)}Staging push details:\n\n${stdoutMod}`
         );
         resolve({
           status: 'success',
@@ -100,17 +100,8 @@ class S3PublishClass {
         });
       });
     } catch (errResult) {
-      if (
-        errResult.hasOwnProperty('code') ||
-        errResult.hasOwnProperty('signal') ||
-        errResult.hasOwnProperty('killed')
-      ) {
-        logger.save(
-          `${'(stage)'.padEnd(15)}failed with code: ${errResult.code}`
-        );
-        logger.save(`${'(stage)'.padEnd(15)}stdErr: ${errResult.stderr}`);
-        throw errResult;
-      }
+      logger.save(`${'(stage)'.padEnd(15)}stdErr: ${errResult.stderr}`);
+      throw errResult;
     }
   }
 }
