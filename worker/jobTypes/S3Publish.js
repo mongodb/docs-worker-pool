76b698b3853dd3c5c7565d42bc549c09e8cf840e
madelinezec <mez2113@columbia.edu>
2020-02-12 10:25:28 -0500

    rever to upstream/stage

--- worker/jobTypes/S3Publish.js
+++ worker/jobTypes/S3Publish.js
@@ -69,13 +69,13 @@ class S3PublishClass {
       `. /venv/bin/activate`,
       `cd repos/${this.GitHubJob.getRepoDirName()}`,
       `make publish`,
-      `make deploy`
+      `make stage`
     ];
 
     // check if need to build next-gen
     if (this.GitHubJob.buildNextGen()) {
       deployCommands[deployCommands.length - 2] = 'make next-gen-publish';
-      deployCommands[deployCommands.length - 1] = 'make next-gen-deploy';
+      deployCommands[deployCommands.length - 1] = 'make next-gen-stage';
     }
 
     logger.save(`${'(stage)'.padEnd(15)}Pushing to staging`);
