0f6a2c3a77de62634429dfae2b9d66881134317b
Sue K <sue.kerschbaumer@10gen.com>
2020-02-20 03:36:40 -0500

    elint

--- worker/jobTypes/githubJob.js
+++ worker/jobTypes/githubJob.js
@@ -3,6 +3,7 @@ const workerUtils = require('../utils/utils');
 const simpleGit = require('simple-git/promise');
 const request = require('request');
 
+
 class GitHubJobClass {
     // pass in a job payload to setup class
     constructor(currentJob) {
