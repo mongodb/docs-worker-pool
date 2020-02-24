c03ce8f168566a0cef08907b758f71966cf02e72
madelinezec <mez2113@columbia.edu>
2020-02-24 10:54:19 -0500

    published branches check

--- StitchApp/services/deployRepoOptions (http)/deployRepoOptions.js
+++ StitchApp/services/deployRepoOptions (http)/deployRepoOptions.js
@@ -11,21 +11,24 @@ exports = async function(payload, response) {
   if (!slackAuth || slackAuth.status !== 'success') {
     return slackAuth;
   }
-  
+
   // get repo options for this user from slack and send over
   var entitlement = await context.functions.execute("getUserEntitlements", payload);
+
   if (!entitlement || entitlement.status !== 'success') {
     return 'ERROR: you are not entitled to deploy any docs repos';
   }
-  
+ 
   // modify list for slack dropdown
   const repos = [];
+
   for (let i = 0; i < entitlement.repos.length; i++) {
     // get published branches for this repo
+    console.log(entitlement.repos[i].split('/')[0])
     let pubBranches = [];
+    
     const thisRepo = entitlement.repos[i];
-    const repoOwner = 'danielborowski'; // CHANGE THIS, hardcoded for now until published-branches.yaml file exists
-    const repoName = thisRepo.split('/')[1];
+    const [repoOwner, repoName] = thisRepo.split('/');
     const getPubBranches = await httpService.get({ 
       url: `https://raw.githubusercontent.com/${repoOwner}/${repoName}/meta/published-branches.yaml`
     });
@@ -51,66 +54,66 @@ exports = async function(payload, response) {
     }
   }
 
-  const block = {
+const  block = {
     "trigger_id": payload.query.trigger_id,
-  	"view": {
-  	  "type": "modal",
-    	"title": {
-    		"type": "plain_text",
-    		"text": "Deploy Docs",
-    		"emoji": true
-    	},
-    	"submit": {
-    		"type": "plain_text",
-    		"text": "Submit",
-    		"emoji": true
-    	},
-    	"close": {
-    		"type": "plain_text",
-    		"text": "Cancel",
-    		"emoji": true
-    	},
-    	"blocks": [
-    		{
-    			"type": "input",
-    			"block_id": "block_repo_option",
-    			"element": {
-    				"type": "multi_static_select",
-    				"action_id": "repo_option",
-    				"placeholder": {
-    					"type": "plain_text",
-    					"text": "Select a repo to deploy",
-    					"emoji": true
-    				},
-    				"options": repos,
-    			},
-    			"label": {
-    				"type": "plain_text",
-    				"text": "Select Repo",
-    				"emoji": true
-    			}
-    		},
-    		{
-    			"type": "input",
-    			"block_id": "block_hash_option",
-    			"element": {
-    				"type": "plain_text_input",
-    				"action_id": "hash_option",
-    				"placeholder": {
-    					"type": "plain_text",
-    					"text": "Enter a commit hash (defaults to latest master commit)"
-    				}
-    			},
-    			"optional": true,
-    			"label": {
-    				"type": "plain_text",
-    				"text": "Commit Hash"
-    			}
-    		}
-    	]
-  	}
+    "view": {
+      "type": "modal",
+      "title": {
+        "type": "plain_text",
+        "text": "Deploy Docs",
+        "emoji": true
+      },
+      "submit": {
+        "type": "plain_text",
+        "text": "Submit",
+        "emoji": true
+      },
+      "close": {
+        "type": "plain_text",
+        "text": "Cancel",
+        "emoji": true
+      },
+      "blocks": [
+        {
+          "type": "input",
+          "block_id": "block_repo_option",
+          "element": {
+            "type": "multi_static_select",
+            "action_id": "repo_option",
+            "placeholder": {
+              "type": "plain_text",
+              "text": "Select a repo to deploy",
+              "emoji": true
+            },
+            "options": repos,
+          },
+          "label": {
+            "type": "plain_text",
+            "text": "Select Repo",
+            "emoji": true
+          }
+        },
+        {
+          "type": "input",
+          "block_id": "block_hash_option",
+          "element": {
+            "type": "plain_text_input",
+            "action_id": "hash_option",
+            "placeholder": {
+              "type": "plain_text",
+              "text": "Enter a commit hash (defaults to latest master commit)"
+            }
+          },
+          "optional": true,
+          "label": {
+            "type": "plain_text",
+            "text": "Commit Hash"
+          }
+        }
+      ]
+    }
   };
-  
+
   const http = context.services.get("slackHTTPService");
   const token = context.values.get("slack_token");
   
