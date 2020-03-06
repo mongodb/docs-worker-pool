eba371d910fc1431527b22d6fe3f4aa78efeef08
madelinezec <mez2113@columbia.edu>
2020-03-06 14:09:45 -0500

    clean up

--- worker/tests/unit/__mocks__/child_process.js
+++ worker/tests/unit/__mocks__/child_process.js
@@ -4,26 +4,8 @@ const path = require('path');
 
 const child_process = jest.genMockFromModule('child_process');
 
-// This is a custom function that our tests can use during setup to specify
-// what the files on the "mock" filesystem should look like when any of the
-// `fs` APIs are used.
-let mockFiles = Object.create(null);
-function __setMockFiles(newMockFiles) {
-  mockFiles = Object.create(null);
-  for (const file in newMockFiles) {
-    const dir = path.dirname(file);
-
-    if (!mockFiles[dir]) {
-      mockFiles[dir] = [];
-    }
-    mockFiles[dir].push(path.basename(file));
-  }
-}
-
-// A custom version of `readdirSync` that reads from the special mocked out
-// file list set via __setMockFiles
+// A custom version of `exec` 
 function exec(commands) {
-  console.log("yooo!!!")
   return 'git@github.com:mongodb/docs-bi-connector.git\n';
 }
 
