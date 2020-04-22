58282e8211cbcc8182f0de3d7713484e5955ca54
Daniel <daniel.borowski@10gen.com>
2020-04-22 15:03:50 -0400

    Update gatsby-config.js

--- configfiles/docs-datalake/gatsby-config.js
+++ configfiles/docs-datalake/gatsby-config.js
@@ -1,23 +1,7 @@
-const userInfo = require('os').userInfo;
-const { getGitBranch } = require('./src/utils/get-git-branch');
-
-const runningEnv = process.env.NODE_ENV || 'production';
-
-require('dotenv').config({
-  path: `.env.${runningEnv}`,
-});
-
-const metadata = {
-  parserBranch: process.env.GATSBY_PARSER_BRANCH,
-  project: process.env.GATSBY_SITE,
-  snootyBranch: getGitBranch(),
-  user: userInfo().username,
-};
+const { siteMetadata } = require('./src/utils/site-metadata');
 
 module.exports = {
   pathPrefix: 'datalake',
-  plugins: ['gatsby-plugin-react-helmet'],
-  siteMetadata: {
-    ...metadata,
-  },
+  plugins: ['gatsby-plugin-react-helmet', `gatsby-plugin-emotion`],
+  siteMetadata,
 };
