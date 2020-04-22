e6e73cfb1770d2f22d272d098c3078e364b2e077
Daniel <daniel.borowski@10gen.com>
2020-04-22 15:05:18 -0400

    Update gatsby-config.js

--- configfiles/devhub-content/gatsby-config.js
+++ configfiles/devhub-content/gatsby-config.js
@@ -1,19 +1,13 @@
 const { getMetadata } = require('./src/utils/get-metadata');
-
-const runningEnv = process.env.NODE_ENV || 'production';
-
-require('dotenv').config({
-    path: `.env.${runningEnv}`,
-});
+const { siteMetadata } = require('./src/utils/site-metadata');
 
 const metadata = getMetadata();
 
 module.exports = {
-    pathPrefix: 'devhub',
-    plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-emotion'],
-    siteMetadata: {
+  pathPrefix: 'devhub',
+  plugins: ['gatsby-plugin-react-helmet', `gatsby-plugin-emotion`],
+  siteMetadata: {
         ...metadata,
         title: 'MongoDB Developer Hub',
     },
 };
-
