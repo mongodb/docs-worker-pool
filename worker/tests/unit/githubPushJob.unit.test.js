e8ac3f05af0f570c385d1a1444e7c62cf2e829fb
madelinezec <maddie.zechar@mongodb.com>
2020-05-12 17:10:41 -0400

    uncomment tests

--- worker/tests/unit/githubPushJob.unit.test.js
+++ worker/tests/unit/githubPushJob.unit.test.js
@@ -87,29 +87,29 @@ describe('Test Class', () => {
 
 //  Tests for build() function
 
-  // it('build() rejects properly killed', async () => {
-  //   const execMock = jest.fn().mockRejectedValue({ killed: true });
-  //   workerUtils.getExecPromise = jest.fn().mockReturnValue(execMock);
-  //   await expect(job.runGithubPush(testPayloadWithRepo)).rejects.toEqual({
-  //     killed: true
-  //   });
-  // });
+  it('build() rejects properly killed', async () => {
+    const execMock = jest.fn().mockRejectedValue({ killed: true });
+    workerUtils.getExecPromise = jest.fn().mockReturnValue(execMock);
+    await expect(job.runGithubPush(testPayloadWithRepo)).rejects.toEqual({
+      killed: true
+    });
+  });
 
-  // it('build() rejects properly code', async () => {
-  //   const execMock = jest.fn().mockRejectedValue({ code: true });
-  //   workerUtils.getExecPromise = jest.fn().mockReturnValue(execMock);
-  //   await expect(job.runGithubPush(testPayloadWithRepo)).rejects.toEqual({
-  //     code: true
-  //   });
-  // });
+  it('build() rejects properly code', async () => {
+    const execMock = jest.fn().mockRejectedValue({ code: true });
+    workerUtils.getExecPromise = jest.fn().mockReturnValue(execMock);
+    await expect(job.runGithubPush(testPayloadWithRepo)).rejects.toEqual({
+      code: true
+    });
+  });
 
-  // it('build() rejects properly signal', async () => {
-  //   const execMock = jest.fn().mockRejectedValue({ signal: true });
-  //   workerUtils.getExecPromise = jest.fn().mockReturnValue(execMock);
-  //   await expect(job.runGithubPush(testPayloadWithRepo)).rejects.toEqual({
-  //     signal: true
-  //   });
-  // });
+  it('build() rejects properly signal', async () => {
+    const execMock = jest.fn().mockRejectedValue({ signal: true });
+    workerUtils.getExecPromise = jest.fn().mockReturnValue(execMock);
+    await expect(job.runGithubPush(testPayloadWithRepo)).rejects.toEqual({
+      signal: true
+    });
+  });
   
   //test
   it('buildRepo() rejects', async () => {
@@ -132,58 +132,58 @@ describe('Test Class', () => {
     await expect(job.runGithubPush(testPayloadWithDevRepo)).rejects.toThrow(mockError2); 
   });
 
-//   it('build() resolves properly notsignal', async () => {
-//     const execMock = jest.fn().mockRejectedValue({ notSignal: true });
-//     workerUtils.getExecPromise = jest.fn().mockReturnValue(execMock);
-//     await expect(
-//       job.runGithubPush(testPayloadWithRepo)
-//     ).rejects.toEqual({
-//       notSignal: true
-//     });
-//   });
+  it('build() resolves properly notsignal', async () => {
+    const execMock = jest.fn().mockRejectedValue({ notSignal: true });
+    workerUtils.getExecPromise = jest.fn().mockReturnValue(execMock);
+    await expect(
+      job.runGithubPush(testPayloadWithRepo)
+    ).rejects.toEqual({
+      notSignal: true
+    });
+  });
 
-//   // Tests for RunGithubPush Function
-//   it('runGithubPush(): no repository name --> should fail to run', async () => {
-//     job.build = jest.fn().mockRejectedValue(error);
-//     await expect(job.runGithubPush({})).rejects.toEqual(error);
-//     jest.runAllTimers();
-//     expect(job.build).toHaveBeenCalledTimes(0);
-//   });
+  // Tests for RunGithubPush Function
+  it('runGithubPush(): no repository name --> should fail to run', async () => {
+    job.build = jest.fn().mockRejectedValue(error);
+    await expect(job.runGithubPush({})).rejects.toEqual(error);
+    jest.runAllTimers();
+    expect(job.build).toHaveBeenCalledTimes(0);
+  });
 
-//   it('runGithubPush(): no branch name --> should fail to run', async () => {
-//     job.build = jest.fn().mockRejectedValue(error);
-//     await expect(job.runGithubPush(testPayloadWithoutBranch)).rejects.toEqual(
-//       error
-//     );
-//     jest.runAllTimers();
-//     expect(job.build).toHaveBeenCalledTimes(0);
-//   });
+  it('runGithubPush(): no branch name --> should fail to run', async () => {
+    job.build = jest.fn().mockRejectedValue(error);
+    await expect(job.runGithubPush(testPayloadWithoutBranch)).rejects.toEqual(
+      error
+    );
+    jest.runAllTimers();
+    expect(job.build).toHaveBeenCalledTimes(0);
+  });
 
-//   it('runGithubPush(): If build fails --> should reject', async () => {
-//     job.build = jest.fn().mockRejectedValue('build failed');
-//     job.cleanup = jest.fn().mockResolvedValue();
-//     job.cloneRepo = jest.fn().mockResolvedValue();
+  it('runGithubPush(): If build fails --> should reject', async () => {
+    job.build = jest.fn().mockRejectedValue('build failed');
+    job.cleanup = jest.fn().mockResolvedValue();
+    job.cloneRepo = jest.fn().mockResolvedValue();
 
-//     await expect(job.runGithubPush({})).rejects.toEqual(error);
+    await expect(job.runGithubPush({})).rejects.toEqual(error);
 
-//     expect(job.cloneRepo).toHaveBeenCalledTimes(0);
-//     expect(job.cleanup).toHaveBeenCalledTimes(0);
-//   });
+    expect(job.cloneRepo).toHaveBeenCalledTimes(0);
+    expect(job.cleanup).toHaveBeenCalledTimes(0);
+  });
 
-//   //sanitize
-//   it('sanitize(): If repo invalid --> should reject', async () => {
-//     job.safeGithubPush = jest.fn().mockRejectedValue(error);
-//     await expect(job.safeGithubPush(testPayloadBadRepo)).rejects.toEqual(error);
-//   });
+  //sanitize
+  it('sanitize(): If repo invalid --> should reject', async () => {
+    job.safeGithubPush = jest.fn().mockRejectedValue(error);
+    await expect(job.safeGithubPush(testPayloadBadRepo)).rejects.toEqual(error);
+  });
 
-//   it('sanitize(): If branch invalid --> should reject', async () => {
-//     await expect(job.safeGithubPush(testPayloadBadBranch)).rejects.toEqual(
-//       error
-//     );
-//   });
-//   it('sanitize(): If owner invalid --> should reject', async () => {
-//     await expect(job.safeGithubPush(testPayloadBadOwner)).rejects.toEqual(
-//       error
-//     );
-//   });
+  it('sanitize(): If branch invalid --> should reject', async () => {
+    await expect(job.safeGithubPush(testPayloadBadBranch)).rejects.toEqual(
+      error
+    );
+  });
+  it('sanitize(): If owner invalid --> should reject', async () => {
+    await expect(job.safeGithubPush(testPayloadBadOwner)).rejects.toEqual(
+      error
+    );
+  });
  });
