8b575cd9dd16227f1f520c6f3114f866d4269d37
Daniel Borowski <danielborowski@Daniels-MBP-5.fios-router.home>
2020-06-04 14:57:10 -0400

    split into two commands

--- worker/jobTypes/S3Publish.js
+++ worker/jobTypes/S3Publish.js
@@ -55,22 +55,40 @@ class S3PublishClass {
 
   async pushToProduction(logger) {
     logger.save(`${'(prod)'.padEnd(15)}Pushing to production`);
-    const deployCommands = [
+
+    const publishCommands = [
       '. /venv/bin/activate',
       `cd repos/${this.GitHubJob.getRepoDirName()}`,
       'make publish',
-      'make deploy',
     ];
+
+    // this is the final command to deploy
+    // will either return summary message from mut or json
+    let deployCommand = 'make deploy';
 
     // check if need to build next-gen
     if (this.GitHubJob.buildNextGen()) {
-      deployCommands[deployCommands.length - 2] = 'make next-gen-publish';
-      deployCommands[deployCommands.length - 1] = 'make next-gen-deploy';
+      publishCommands[publishCommands.length - 1] = 'make next-gen-publish';
+      deployCommand = 'make next-gen-deploy';
     }
 
+    // first publish
     try {
       const exec = workerUtils.getExecPromise();
-      const command = deployCommands.join(' && ');
+      const command = publishCommands.join(' && ');
+      const { stdout } = await exec(command);
+      logger.save(
+        `${'(prod)'.padEnd(15)}Production publish details:\n\n${stdout}`
+      );
+    } catch (errResult) {
+      logger.save(`${'(prod)'.padEnd(15)}stdErr: ${errResult.stderr}`);
+      throw errResult;
+    }
+
+    // finally deploy site
+    try {
+      const exec = workerUtils.getExecPromise();
+      const command = deployCommand;
       const { stdout } = await exec(command);
       let stdoutMod = stdout;
 
@@ -116,7 +134,7 @@ class S3PublishClass {
         });
       });
     } catch (errResult) {
-      logger.save(`${'(stage)'.padEnd(15)}stdErr: ${errResult.stderr}`);
+      logger.save(`${'(prod)'.padEnd(15)}stdErr: ${errResult.stderr}`);
       throw errResult;
     }
   }
