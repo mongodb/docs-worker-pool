d87fff9efd40cf83cdcd180ac496a9e8b9a97962
madelinezec <mez2113@columbia.edu>
2020-08-12 00:22:04 -0400

    Update gatsby-config.js

--- configfiles/docs-node/gatsby-config.js
+++ configfiles/docs-node/gatsby-config.js
@@ -1,7 +1,7 @@
 const { siteMetadata } = require('./src/utils/site-metadata');
 
 module.exports = {
-  pathPrefix: '/node',
+  pathPrefix: 'drivers/node',
   plugins: ['gatsby-plugin-react-helmet', `gatsby-plugin-emotion`],
   siteMetadata,
 };
