40fdb9925b766703e197b3d7d7e37a2374e3404d
Skerschb <sue.kerschbaumer@10gen.com>
2020-08-18 22:41:42 -0400

    run tests

--- worker/tests/unit/productionDeployJob.unit.test.js
+++ worker/tests/unit/productionDeployJob.unit.test.js
@@ -1,25 +1,37 @@
 const deployjob = require('../../jobTypes/productionDeployJob');
 const workerUtils = require('../../utils/utils');
-const githubJob = require('../../jobTypes/githubJob').GitHubJobClass;
+const GitHubJob = require('../../jobTypes/githubJob').GitHubJobClass;
+
+const payloadObj = {
+  repoName: 'docs_build_test',
+  branchName: 'DOCSP-test',
+  repoOwner: 'mongodb',
+};
+
+const testPayloadWithRepo = {
+  payload: payloadObj,
+};
 
 describe('Test Class', () => {
-    // Dont actually reset the directory and dont care about the logging
-    beforeAll(() => {
-        workerUtils.resetDirectory = jest.fn().mockResolvedValue();
-        workerUtils.logInMongo = jest.fn().mockResolvedValue();
-        githubJob.buildRepo = jest.fn().mockReturnValueOnce({
-            status: 'success',
-            stdout: null,
-            stderr: null,
-        });
-        jest.useFakeTimers();
-    });
+  // Dont actually reset the directory and dont care about the logging
+  beforeAll(() => {
+    workerUtils.resetDirectory = jest.fn().mockResolvedValue();
+    workerUtils.logInMongo = jest.fn().mockResolvedValue();
+    const githubJob = new GitHubJob(testPayloadWithRepo);
 
-    it('startGithubBuild(: If build is successful --> return true', async () => {
-        try {
-            deployjob.startGithubBuild(null, null);
-        } catch (e) {
-            throw e;
-        }
+    githubJob.buildRepo = jest.fn().mockReturnValueOnce({
+      status: 'success',
+      stdout: null,
+      stderr: null,
     });
+    jest.useFakeTimers();
+  });
+
+  it('startGithubBuild(: If build is successful --> return true', async () => {
+    try {
+      deployjob.startGithubBuild(null, null);
+    } catch (e) {
+      throw e;
+    }
+  });
 });
