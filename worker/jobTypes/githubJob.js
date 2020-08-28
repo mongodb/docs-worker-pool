88ce76293e4fc87a11aa1dec938499a223014d64
madelinezec <maddie.zechar@mongodb.com>
2020-08-28 10:55:24 -0400

    revert

--- worker/jobTypes/githubJob.js
+++ worker/jobTypes/githubJob.js
@@ -119,7 +119,7 @@ class GitHubJobClass {
 
     // our maintained directory of makefiles
     async downloadMakefile() {
-        const makefileLocation = `https://raw.githubusercontent.com/madelinezec/docs-worker-pool/test-meta/makefiles/Makefile.${this.currentJob.payload.repoName}`;
+        const makefileLocation = `https://raw.githubusercontent.com/mongodb/docs-worker-pool/test-meta/makefiles/Makefile.${this.currentJob.payload.repoName}`;
         const returnObject = {};
         return new Promise(function(resolve, reject) {
             request(makefileLocation, function(error, response, body) {
