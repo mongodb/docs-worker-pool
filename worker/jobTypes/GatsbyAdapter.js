83d862de014a12fd7628e58630eee8694631a508
madelinezec <maddie.zechar@mongodb.com>
2020-09-03 11:25:33 -0400

    fix bug for path prefix consisting of empty string

--- worker/jobTypes/GatsbyAdapter.js
+++ worker/jobTypes/GatsbyAdapter.js
@@ -10,10 +10,11 @@ class GatsbyAdapterClass {
     let server_user = await workerUtils.getServerUser();
     let envVars = `GATSBY_PARSER_USER=${server_user}\nGATSBY_PARSER_BRANCH=${this.GitHubJob.currentJob.payload.branchName}\n`;
     const pathPrefix = this.GitHubJob.currentJob.payload.pathPrefix;
-
-    if(pathPrefix){
+		
+    if(typeof pathPrefix !== 'undefined' && pathPrefix !== null){
       envVars += `PATH_PREFIX=${pathPrefix}\n`
-    }
+		}
+
     return envVars
   }
 
