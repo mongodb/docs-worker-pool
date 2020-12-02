b3d3a0b4cbdb7cab1c673844699a3b877ffe8990
madelinezec <mez2113@columbia.edu>
2020-12-02 10:53:34 -0500

    Update githubJob.js

--- worker/jobTypes/githubJob.js
+++ worker/jobTypes/githubJob.js
@@ -50,7 +50,7 @@ class GitHubJobClass {
         const repoContent = await workerUtils.getRepoPublishedBranches(repoObject)
         const server_user = await workerUtils.getServerUser()
         let pathPrefix; 
-        let manifestPrefix;
+
         if(isProdDeployJob){
           //versioned repo
           if(repoContent && repoContent.content.version.active.length > 1){
@@ -72,7 +72,6 @@ class GitHubJobClass {
           this.currentJob.payload.pathPrefix = pathPrefix;
           const mutPrefix = pathPrefix.split(`/${server_user}`)[0];
           this.currentJob.payload.mutPrefix = mutPrefix;
-          this.currentJob.payload.manifestPrefix = manifestPrefix;
         }
       }catch(error){
         console.log(error)
@@ -122,7 +121,7 @@ class GitHubJobClass {
 
     // our maintained directory of makefiles
     async downloadMakefile() {
-        const makefileLocation = `https://raw.githubusercontent.com/madelinezec/docs-worker-pool/meta/makefiles/Makefile.${this.currentJob.payload.repoName}`;
+        const makefileLocation = `https://raw.githubusercontent.com/mongodb/docs-worker-pool/meta/makefiles/Makefile.${this.currentJob.payload.repoName}`;
         const returnObject = {};
         return new Promise(function(resolve, reject) {
             request(makefileLocation, function(error, response, body) {
