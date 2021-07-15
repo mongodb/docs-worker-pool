d0d4f08dd77ba31e82f89d49d507d4ff9a5a160e
Allison Reinheimer Moore <allison@mongodb.com>
2021-07-15 10:39:38 -0400

    clarify special cases

--- makefiles/shared.mk
+++ makefiles/shared.mk
@@ -63,7 +63,9 @@ next-gen-stage: ## Host online for review
 endif
 
 ## Update the search index for this branch
-## Manual has its own custom search index, because it is special. Landing doesn't have search at all.
+## HACK-Y WORKAROUNDS:
+## Manual && Atlas have own search indexes to rename their manifests because of a bug in the workerpool code
+## Landing doesn't have search at all.
 ifndef CUSTOM_SEARCH_INDEX
 next-gen-deploy-search-index:
 	@echo "Building search index"
