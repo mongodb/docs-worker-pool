9436ed57611936cc668f3f655486b00187d7d5a5
Allison Reinheimer Moore <allison@mistressofinformation.com>
2021-07-15 10:45:31 -0400

    (TEMP WORKAROUND) DOP-2343: exclude landing and rename cloud-docs to Atlas (#468)
    * DOP-2343: exclude docs-landing from search manifests
    
    * DOP-2343: (TEMP HACK) rename cloud-docs manifest to atlas
    
    * clarify special cases
    
    * also for cloudgov
    
    * rename custom index var

--- makefiles/shared.mk
+++ makefiles/shared.mk
@@ -63,7 +63,9 @@ next-gen-stage: ## Host online for review
 endif
 
 ## Update the search index for this branch
-## Manual has its own custom search index, because it is special.
+## HACK-Y WORKAROUNDS:
+## docs && cloud-docs && cloudgov have own search indexes to rename their manifests because of a bug in the workerpool code
+## Landing doesn't have search at all.
 ifndef CUSTOM_SEARCH_INDEX
 next-gen-deploy-search-index:
 	@echo "Building search index"
