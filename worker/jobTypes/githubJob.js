4ce0650543793f6e9388114a8b110fa0311894b3
Kumaraguru Periyasamy Kanthasamy <48932104+GuruPKK@users.noreply.github.com>
2021-09-03 06:47:29 -0700

    Update githubJob.js

--- worker/jobTypes/githubJob.js
+++ worker/jobTypes/githubJob.js
@@ -340,7 +340,6 @@ class GitHubJobClass {
         commandsToBuild[commandsToBuild.length - 1] = 'make get-build-dependencies';
         commandsToBuild.push('make next-gen-html')
         if (this.currentJob.payload.repoName === 'devhub-content') {
-            commandsToBuild[commandsToBuild.length - 1] += ` STRAPI_PUBLICATION_STATE=preview`;
             commandsToBuild[commandsToBuild.length - 1] += ` PATH_PREFIX=/developer`;
           }
           
