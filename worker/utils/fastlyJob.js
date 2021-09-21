679af00ec61f8b7416e330df7c84fd391d7a4823
Kumaraguru Periyasamy Kanthasamy <48932104+GuruPKK@users.noreply.github.com>
2021-09-21 14:34:03 -0700

    Dochub service id fix (#510)
    * Fix DOCHUB Service ID

--- worker/utils/fastlyJob.js
+++ worker/utils/fastlyJob.js
@@ -1,17 +1,8 @@
 const axios = require('axios').default;
 const environment = require('../utils/environment').EnvironmentClass;
-const fastly = require('fastly')(environment.getFastlyToken());
+const fastly = require('fastly');
 const utils = require('../utils/utils');
 const Logger = require('../utils/logger').LoggerClass;
-
-const fastlyServiceId = environment.getFastlyServiceId();
-const headers = {
-    'Fastly-Key': environment.getFastlyToken(),
-    'Accept': 'application/json',
-    'Content-Type': 'application/json',
-    'Fastly-Debug': 1,
-};
-
 
 class FastlyJobClass {
     // pass in a job payload to setup class
@@ -23,86 +14,74 @@ class FastlyJobClass {
         }
     }
 
+    getHeaders(token) {
+        return  {
+            'Accept': 'application/json',
+            'Fastly-Key': token,
+            'Content-Type': 'application/json',
+            'Fastly-Debug': 1,
+        };
+    }
+
     // takes in an array of surrogate keys and purges cache for each
     async purgeCache(urlArray, logger, purgeAll = false) {
+        const token = environment.getFastlyToken(this.currentJob.currentJob.payload.repoName);
+        const serviceId = environment.getFastlyServiceId(this.currentJob.currentJob.payload.repoName);
         if (!Array.isArray(urlArray)) {
+            console.log(`ERROR urlArray ${this.currentJob.currentJob._id}`)
             throw new Error('Parameter `urlArray` needs to be an array of urls');
         }
-        if (!purgeAll) {
+         if (!purgeAll) {
             try {
                 logger.save(`Purging URL's`);
                 //retrieve surrogate key associated with each URL/file updated in push to S3
-                const surrogateKeyPromises = urlArray.map(url => this.retrieveSurrogateKey(url));
-                const surrogateKeyArray = await Promise.all(surrogateKeyPromises)
-
-                //purge each surrogate key
-                const purgeRequestPromises = surrogateKeyArray.map(surrogateKey => this.requestPurgeOfSurrogateKey(surrogateKey));
-                await Promise.all(purgeRequestPromises);
+                const purgePromises = urlArray.map(url => this.purgeURL(url));
+                await Promise.all(purgePromises)
+                console.log(`Purging URL purge urls completed Total URL's: ${purgePromises.length}`);
                 // GET request the URLs to warm cache for our users
                 const warmCachePromises = urlArray.map(url => this.warmCache(url));
                 await Promise.all(warmCachePromises)
             } catch (error) {
                 logger.save(`${'(prod)'.padEnd(15)}error in purge urls: ${error}`);
+                console.log(`Error purge URLs ${error}`);
             }
 
         } else {
             try {
                 logger.save(`Purging all`);
-                await this.requestPurgeAll()
-            } catch (error) {
+                await this.requestPurgeAll(serviceId, token)
+            } catch (error) {   
                 logger.save(`${'(prod)'.padEnd(15)}error in purge all: ${error}`);
             }
         }
     }
 
-
-    async retrieveSurrogateKey(url) {
-
+    async purgeURL(url) {
         try {
             return axios({
-                method: 'HEAD',
-                url: url,
-                headers: headers,
+                method: 'PURGE',
+                url: url
             }).then(response => {
                 if (response.status === 200) {
-                    return response.headers['surrogate-key'];
+                    return;
+                } else {
+                    console.log(`purgeURL URL: ${url} invaid response ${response}`);
                 }
             });
         } catch (error) {
-            this.logger.save(`${'(prod)'.padEnd(15)}error in retrieveSurrogateKey: ${error}`);
-            throw error
+            console.log(`purgeURL Failed: ${url} error: ${error}`);
+            this.logger.save(`${'(prod)'.padEnd(15)}error in purgeURL ${url}: ${error}`);
         }
 
     }
 
-    async requestPurgeOfSurrogateKey(surrogateKey) {
-        headers['Surrogate-Key'] = surrogateKey
-
-        try {
-            return axios({
-                method: `POST`,
-                url: `https://api.fastly.com/service/${fastlyServiceId}/purge/${surrogateKey}`,
-                path: `/service/${fastlyServiceId}/purge${surrogateKey}`,
-                headers: headers,
-            })
-                .then(response => {
-                    if (response.status === 200) {
-                        return true
-                    }
-                });
-        } catch (error) {
-            this.logger.save(`${'(prod)'.padEnd(15)}error in requestPurgeOfSurrogateKey: ${error}`);
-            throw error;
-        }
-    }
-
-    async requestPurgeAll() {
+    async requestPurgeAll(fastlyServiceId, token) {
         try {
             return axios({
                 method: `POST`,
                 url: `https://api.fastly.com/service/${fastlyServiceId}/purge_all`,
                 path: `/service/${fastlyServiceId}/purge_all`,
-                headers: headers,
+                headers: this.getHeaders(token),
             })
                 .then(response => {
                     if (response.status === 200) {
@@ -117,31 +96,24 @@ class FastlyJobClass {
 
     // request urls of updated content to "warm" the cache for our customers
     async warmCache(updatedUrl) {
-
         try {
-            return axios.get(updatedUrl)
-                .then(response => {
-                    if (response.status === 200) {
-                        return true;
-                    }
-                })
+            await axios.get(updatedUrl);
         } catch (error) {
+            console.log(`Warmcache failed for ${updatedUrl}`);
             this.logger.save(`${'(prod)'.padEnd(15)}stdErr: ${error}`);
-            throw error;
         }
     }
 
     // upserts {source: target} mappings
     // to the fastly edge dictionary
-    async connectAndUpsert(map) {
+    async connectAndUpsert(map, serviceId, token) {
         const options = {
             item_value: map.target,
         };
-        const connectString = `/service/${fastlyServiceId}/dictionary/${environment.getDochubMap()}/item/${map.source
-            }`;
-
+        const fastlyObj = fastly(token);
+        const connectString = `/service/${serviceId}/dictionary/${environment.getDochubMap()}/item/${map.source}`;
         return new Promise((resolve, reject) => {
-            fastly.request('PUT', connectString, options, (err, obj) => {
+            fastlyObj.request('PUT', connectString, options, (err, obj) => {
                 if (err) reject(err);
                 resolve(obj);
             });
