c76e1e727d5b1a5940de51398243e30b20717576
Cassidy Schaufele <cassidyschaufele@gmail.com>
2021-12-23 13:50:25 -0700

    Add brief ShellCommandExecutor test suite

---
+++ tests/unit/services/shellCommandExecutor.test.ts
@@ -0,0 +1,43 @@
+import { ShellCommandExecutor } from '../../../src/services/commandExecutor';
+import cp from 'child_process';
+jest.mock('child_process');
+
+describe('ShellCommandExecutor Tests', () => {
+    let commandExecutor: ShellCommandExecutor;
+
+    beforeEach(() => {
+        commandExecutor = new ShellCommandExecutor();
+    })
+
+    afterEach(() => {
+        jest.resetAllMocks();
+    })
+
+    test('Construct ShellCommandExecutor', () => {
+        expect(new ShellCommandExecutor()).toBeDefined();
+    })
+
+    describe('ShellCommandExecutor Tests', () => {
+        test('ShellCommandExecutor resp contains stderr on success case (needed for legacy builds)', async() => {
+            cp.exec.mockImplementation((command, options, callback) => {
+                callback(null, { stderr: 'test warning', stdout: 'test success' });
+            });
+            let resp = await commandExecutor.execute([]);
+            expect(resp.error).toBe('test warning');
+            expect(resp.output).toBe('test success');
+            expect(resp.status).toBe('success');
+            expect(cp.exec).toBeCalledTimes(1);
+        })
+
+        test('ShellCommandExecutor properly throws on system level error', async() => {
+            cp.exec.mockImplementation((command, options, callback) => {
+                callback(Error("Test error"), {stdErr: "invalid command", stdout: 'test_repo_project_snooty_name' });
+            });
+            let resp = await commandExecutor.execute([]);
+            expect(resp.error).not.toBe(undefined);
+            expect(resp.output).toBe(null);
+            expect(resp.status).toBe('failed');
+            expect(cp.exec).toBeCalledTimes(1);
+        })
+    })
+})
