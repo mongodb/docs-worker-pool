e7ae90379e4f9993c76cbf23b14b6a2ea45b2510
Kumaraguru Periyasamy Kanthasamy <48932104+GuruPKK@users.noreply.github.com>
2022-01-09 22:28:25 -0800

    Update commandExecutor.ts

--- src/services/commandExecutor.ts
+++ src/services/commandExecutor.ts
@@ -16,31 +16,24 @@ const execWithOptions: (
 let exec = promisify(execWithOptions) as any;
 
 export class CommandExecutorResponse {
-  status: string;
-  output: any | null;
-  error: any | null;
+    status: string;
+    output: any | null;
+    error: any | null;
 }
 
 export interface ICommandExecutor {
-  execute(commands: Array<string>): Promise<CommandExecutorResponse>;
+    execute(commands: Array<string>): Promise<CommandExecutorResponse>;
 }
 
 export interface IJobCommandExecutor extends ICommandExecutor {
-  getServerUser(): Promise<CommandExecutorResponse>;
+    getSnootyProjectName(repoDirName): Promise<CommandExecutorResponse>;
+    getServerUser(): Promise<CommandExecutorResponse>;
 }
 
 export interface IGithubCommandExecutor {
-  checkoutBranchForSpecificHead(
-    repoDirName: string,
-    branchName: string,
-    commitHash: string
-  ): Promise<CommandExecutorResponse>;
-  pullRepo(
-    repoDirName: string,
-    branchName: string,
-    commitHash: string | null | undefined
-  ): Promise<CommandExecutorResponse>;
-  applyPatch(repoDirName: string, patchName: string): Promise<CommandExecutorResponse>;
+    checkoutBranchForSpecificHead(repoDirName:string, branchName:string, commitHash:string): Promise<CommandExecutorResponse>;
+    pullRepo(repoDirName:string, branchName:string, commitHash:string| null | undefined): Promise<CommandExecutorResponse>;
+    applyPatch(repoDirName:string, patchName:string): Promise<CommandExecutorResponse>;
 }
 
 export class ShellCommandExecutor implements ICommandExecutor {
@@ -64,46 +57,53 @@ export class ShellCommandExecutor implements ICommandExecutor {
         }
         return resp;
     }
-    return resp;
-  }
 }
 
 export class JobSpecificCommandExecutor extends ShellCommandExecutor implements IJobCommandExecutor {
-  async getServerUser(): Promise<CommandExecutorResponse> {
-    return await this.execute(['whoami']);
-  }
+
+    async getSnootyProjectName(repoDirName: any): Promise<CommandExecutorResponse> {
+        const commands = [
+            `. /venv/bin/activate`,
+            `cd ~/repos/${repoDirName}`,
+            `make get-project-name`
+        ];
+        return await this.execute(commands);
+    }
+
+    async getServerUser(): Promise<CommandExecutorResponse> {
+        return await this.execute(["whoami"]);
+    }
 }
 
 export class GithubCommandExecutor extends ShellCommandExecutor implements IGithubCommandExecutor {
-  async applyPatch(repoDirName: string, patchName: string) {
-    const patchCommand = [`cd repos/${repoDirName}`, `patch -p1 < ${patchName}`];
-    return await this.execute(patchCommand);
-  }
+    async applyPatch(repoDirName: string, patchName: string) {
 
-  async checkoutBranchForSpecificHead(
-    repoDirName: string,
-    branchName: string,
-    newHead: string
-  ): Promise<CommandExecutorResponse> {
-    const commitCheckCommands = [
-      `cd repos/${repoDirName}`,
-      `git fetch`,
-      `git checkout ${branchName}`,
-      `git branch ${branchName} --contains ${newHead}`,
-    ];
-
-    return await this.execute(commitCheckCommands);
-  }
-
-  async pullRepo(
-    repoDirName: string,
-    branchName: string,
-    newHead: string | null | undefined = null
-  ): Promise<CommandExecutorResponse> {
-    const pullRepoCommands = [`cd repos/${repoDirName}`, `git checkout ${branchName}`, `git pull origin ${branchName}`];
-    if (newHead) {
-      pullRepoCommands.push(`git checkout ${newHead} .`);
+        const patchCommand = [
+            `cd repos/${repoDirName}`,
+            `patch -p1 < ${patchName}`
+          ];
+        return await this.execute(patchCommand);
     }
-    return await this.execute(pullRepoCommands);
-  }
+
+    async checkoutBranchForSpecificHead(repoDirName: string, branchName: string, newHead: string): Promise<CommandExecutorResponse> {
+        const commitCheckCommands = [
+                `cd repos/${repoDirName}`,
+                `git fetch`,
+                `git checkout ${branchName}`,
+                `git branch ${branchName} --contains ${newHead}`
+            ];
+
+            return await this.execute(commitCheckCommands);
+
+    }
+
+    async pullRepo(repoDirName: string, branchName: string, newHead: string| null | undefined= null): Promise<CommandExecutorResponse> {
+        let pullRepoCommands = [`cd repos/${repoDirName}`, 
+        `git checkout ${branchName}`,
+        `git pull origin ${branchName}`];
+        if (newHead) {
+            pullRepoCommands.push(`git checkout ${newHead} .`)
+        }
+        return await this.execute(pullRepoCommands);
+    }
 }
