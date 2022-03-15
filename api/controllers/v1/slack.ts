8d835372bd86ee7a27e089447bc38188b7a3b662
Kumaraguru Periyasamy Kanthasamy <48932104+GuruPKK@users.noreply.github.com>
2022-03-15 12:19:09 -0700

    Update slack.ts

--- api/controllers/v1/slack.ts
+++ api/controllers/v1/slack.ts
@@ -156,7 +156,7 @@ export const DeployRepo = async (event: any = {}, context: any = {}): Promise<an
     let urlSlug = branchObject.aliasObject.urlSlug; //string or null, string must match value in urlAliases or gitBranchName
     const isStableBranch = branchObject.aliasObject.isStableBranch; // bool or Falsey
     aliases = aliases?.filter((a) => a);
-    if (!urlSlug && non_versioned == false) {
+    if (!urlSlug || urlSlug === '') {
       urlSlug = branchName;
     }
 
@@ -172,7 +172,7 @@ export const DeployRepo = async (event: any = {}, context: any = {}): Promise<an
       hashOption,
       repoInfo.project,
       repoInfo.prefix[c.get<string>('env')],
-      urlSlug ? urlSlug : branchName,
+      urlSlug,
       false,
       false,
       '-g'
