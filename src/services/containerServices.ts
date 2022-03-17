d7d87f096813ca63ae528a0e5133b9832c920821
Kumaraguru Periyasamy Kanthasamy <48932104+GuruPKK@users.noreply.github.com>
2022-03-17 15:34:35 -0700

    Update containerServices.ts

--- src/services/containerServices.ts
+++ src/services/containerServices.ts
@@ -54,7 +54,7 @@ export class ECSContainer implements IContainerServices {
 
     const awsvpcConfig: AwsVpcConfiguration = {
       subnets: this._config.get('subnets'),
-      assignPublicIp: 'DISABLED',
+      assignPublicIp: 'ENABLED',
     };
 
     const networkConfig: NetworkConfiguration = {
