3c531ee61cc12ec566ac816768c526b195bbe019
mango-db <58707794+mango-db@users.noreply.github.com>
2022-04-25 11:38:12 -0400

    DOP-2731-3: Add manifest bucket/folder logging

--- src/job/manifestJobHandler.ts
+++ src/job/manifestJobHandler.ts
@@ -46,6 +46,7 @@ export class ManifestJobHandler extends JobHandler {
     const b = this._config.get<string>('searchIndexBucket') ?? 'docs-search-indexes-test';
     // /deploy -> send to /prd folder. /test-deploy -> send to /preprd folder
     const f = this._config.get<string>('searchIndexFolder') ?? 'test-folder';
+    this.logger.info(this.currJob._id, `Manifest attempt to upload to bucket: ${b}, folder: ${f}`);
     // Due to the dual existence of prefixes, check for both for redundancy
     const maP = this.currJob.manifestPrefix ?? this.currJob.payload.manifestPrefix;
     const muP = this.currJob.mutPrefix ?? this.currJob.payload.mutPrefix;
