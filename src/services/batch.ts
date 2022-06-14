ee009ec1bfb0f9b68c20cf93ab9aa7b1fd8f437b
Rajan Khullar <rajan.khullar@mongodb.com>
2022-06-14 16:24:37 -0400

    lint

--- src/services/batch.ts
+++ src/services/batch.ts
@@ -1,25 +1,29 @@
 import { BatchClient, SubmitJobCommand, SubmitJobCommandOutput } from '@aws-sdk/client-batch';
 
 export class Batch {
-    private readonly environment: string;
-    private readonly client: BatchClient;
+  private readonly environment: string;
+  private readonly client: BatchClient;
 
-    constructor(environment: string) {
-        this.environment = environment;
-        this.client = new BatchClient({});
-    }
+  constructor(environment: string) {
+    this.environment = environment;
+    this.client = new BatchClient({});
+  }
 
-    async submitArchiveJob(sourceBucket: string, targetBucket: string, siteName: string): Promise<SubmitJobCommandOutput> {
-        const command = new SubmitJobCommand({
-            jobName: 'archive',
-            jobQueue: `docs-archive-${this.environment}`,
-            jobDefinition: `docs-archive-${this.environment}`,
-            parameters: {
-                'source-bucket': sourceBucket,
-                'target-bucket': targetBucket,
-                'site-name': siteName
-            }
-        });
-        return await this.client.send(command);
-    }
+  async submitArchiveJob(
+    sourceBucket: string,
+    targetBucket: string,
+    siteName: string
+  ): Promise<SubmitJobCommandOutput> {
+    const command = new SubmitJobCommand({
+      jobName: 'archive',
+      jobQueue: `docs-archive-${this.environment}`,
+      jobDefinition: `docs-archive-${this.environment}`,
+      parameters: {
+        'source-bucket': sourceBucket,
+        'target-bucket': targetBucket,
+        'site-name': siteName,
+      },
+    });
+    return await this.client.send(command);
+  }
 }
