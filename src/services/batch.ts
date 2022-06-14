4aea9e2fabe774c106cdeb8ac31ed0b88c346e1f
Rajan Khullar <rajan.khullar@mongodb.com>
2022-06-14 09:40:25 -0400

    save

--- src/services/batch.ts
+++ src/services/batch.ts
@@ -2,7 +2,24 @@ import { BatchClient, SubmitJobCommand } from '@aws-sdk/client-batch';
 
 export class Batch {
     private client: BatchClient;
+    private readonly environment: string;
+
     constructor() {
-        this.client = new BatchClient();
+        this.client = new BatchClient({});
+        this.environment = 'dev';
+    }
+    async submitArchiveJob(sourceBucket: string, targetBucket: string, siteName: string) {
+        const command = new SubmitJobCommand({
+            jobName: 'archive',
+            jobQueue: `docs-archive-${this.environment}`,
+            jobDefinition: `docs-archive-${this.environment}`,
+            parameters: {
+                'source-bucket': sourceBucket,
+                'target-bucket': targetBucket,
+                'site-name': siteName
+            }
+        });
+        const response = await this.client.send(command);
+        console.log(response);
     }
 }
