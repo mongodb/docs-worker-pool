6a2bcac659b4e06d6f5f7cebbf32d93d64bb794b
Cassidy Schaufele <cassidyschaufele@gmail.com>
2022-09-06 09:16:50 -0600

    fixup

--- modules/persistence/index.ts
+++ modules/persistence/index.ts
@@ -15,7 +15,7 @@ const app = async (path) => {
     const buildId = new mongodb.ObjectId();
     Promise.all([insertEntries(buildId, zip), insertMetadata(buildId, zip)]);
   } catch (error) {
-    console.error(`ERROR: Persistence Module encountered a terminal error: ${error}`);
+    console.error(`Persistence Module encountered a terminal error: ${error}`);
     throw error;
     process.exit(0);
   }
