cd744a55132a9ec2b865decbf13cdb3030222354
rayangler <27821750+rayangler@users.noreply.github.com>
2022-12-14 14:59:29 -0500

    Add console log

--- modules/oas-page-builder/src/services/commandExecutor.ts
+++ modules/oas-page-builder/src/services/commandExecutor.ts
@@ -5,6 +5,7 @@ const execute = promisify(exec);
 
 export const execRedoc = async (spec: string, outputPath: string, redocPath: string) => {
   const command = `node ${redocPath} build ${spec} --output ${outputPath}`;
+  console.log(command);
   const { stdout, stderr } = await execute(command);
   if (stderr) {
     console.error(`Error trying to build page ${outputPath} with Redoc.`);
