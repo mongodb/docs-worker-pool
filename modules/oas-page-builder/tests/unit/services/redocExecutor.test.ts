4f7d7a2fa227bca9f93e5e9888fb5ac63f8d961d
rayangler <27821750+rayangler@users.noreply.github.com>
2022-12-20 10:43:03 -0500

    Remove commented code

--- modules/oas-page-builder/tests/unit/services/redocExecutor.test.ts
+++ modules/oas-page-builder/tests/unit/services/redocExecutor.test.ts
@@ -1,16 +1,6 @@
 import { RedocExecutor } from '../../../src/services/redocExecutor';
 import cp from 'child_process';
 
-// jest.mock('child_process').mockImplementation(() => {});
-
-// const mockCpExec = jest.spyOn(cp, 'exec');
-// // @ts-ignore
-// mockCpExec.mockImplementation(() => {});
-
-// jest.mock('child_process', () => ({
-//   exec: jest.fn(() => Promise.resolve({ stdout: 'foo', stderr: 'bar' }))
-// }));
-
 jest.mock('child_process');
 // @ts-ignore
 cp.exec.mockImplementation((command: string, callback: any) => {
