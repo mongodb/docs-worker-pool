61df145dde40eb4549009fe1c6b98a4b5dab3557
rayangler <27821750+rayangler@users.noreply.github.com>
2023-01-03 14:42:11 -0500

    DOP-3414: Add back button support for OAS Page Builder (#725)

--- modules/oas-page-builder/src/services/pageBuilder.ts
+++ modules/oas-page-builder/src/services/pageBuilder.ts
@@ -1,9 +1,8 @@
 import fetch from 'node-fetch';
-import { ModuleOptions } from '../types';
 import { normalizePath } from '../utils/normalizePath';
-import { execRedoc } from './commandExecutor';
+import { RedocExecutor } from './redocExecutor';
 import { findLastSavedGitHash } from './database';
-import { OASPageMetadata } from './types';
+import { OASPageMetadata, PageBuilderOptions } from './types';
 
 const OAS_FILE_SERVER = 'https://mongodb-mms-prod-build-server.s3.amazonaws.com/openapi/';
 
@@ -53,8 +52,10 @@ const getAtlasSpecUrl = async (apiKeyword: string) => {
 
 export const buildOpenAPIPages = async (
   entries: [string, OASPageMetadata][],
-  { output, redoc: redocPath, repo: repoPath }: ModuleOptions
+  { output, redoc: redocPath, repo: repoPath, siteUrl, siteTitle }: PageBuilderOptions
 ) => {
+  const redocExecutor = new RedocExecutor(redocPath, siteUrl, siteTitle);
+
   for (const [pageSlug, data] of entries) {
     const { source_type: sourceType, source } = data;
 
@@ -73,7 +74,7 @@ export const buildOpenAPIPages = async (
       }
 
       const finalFilename = normalizePath(`${output}/${pageSlug}/index.html`);
-      await execRedoc(spec, finalFilename, redocPath);
+      await redocExecutor.execute(spec, finalFilename);
     } catch (e) {
       console.error(e);
       // Continue to try to build other pages since it's possible that mut will
---
+++ modules/oas-page-builder/tests/unit/services/redocExecutor.test.ts
@@ -0,0 +1,32 @@
+import { RedocExecutor } from '../../../src/services/redocExecutor';
+import cp from 'child_process';
+
+jest.mock('child_process');
+// @ts-ignore
+cp.exec.mockImplementation((command: string, callback: any) => {
+  callback(null, command);
+});
+
+describe('RedocExecutor', () => {
+  it('calls the Redoc build command with expected parameters', async () => {
+    const testRedocPath = '/path/to/redoc/cli/index.js';
+    const testSiteUrl = 'https://mongodb.com/docs';
+    const testSiteTitle = 'Test Docs';
+    const redocExecutor = new RedocExecutor(testRedocPath, testSiteUrl, testSiteTitle);
+
+    const testSpecSource = '/path/to/spec.json';
+    const testOutputPath = '/path/to/output/index.html';
+    await redocExecutor.execute(testSpecSource, testOutputPath);
+
+    const expectedOptions = {
+      customOptions: {
+        backNavigationPath: testSiteUrl,
+        siteTitle: testSiteTitle,
+      },
+    };
+    const expectedCommand = `node ${testRedocPath} build ${testSpecSource} --output ${testOutputPath} --options '${JSON.stringify(
+      expectedOptions
+    )}'`;
+    expect(cp.exec).toBeCalledWith(expectedCommand, expect.anything());
+  });
+});
