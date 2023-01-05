5e5f85bf9efe9620984cb9f6c5c78a51bd770de0
rayangler <27821750+rayangler@users.noreply.github.com>
2023-01-05 15:19:28 -0500

    Remove temporary comment

--- modules/oas-page-builder/index.ts
+++ modules/oas-page-builder/index.ts
@@ -18,7 +18,6 @@ program
 program.parse();
 const options = program.opts<ModuleOptions>();
 
-// TODO-3431: Delete this comment. Temporary comment to trigger GH workflow
 const app = async (options: ModuleOptions) => {
   const { bundle: bundlePath } = options;
   const metadata = getOASMetadata(bundlePath);
