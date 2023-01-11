a35812bb8b9c9d78880d4b8bfaaac5692e1ff80d
Seung Park <seung.park@mongodb.com>
2023-01-10 19:12:04 -0500

    ensure parser called before persist data

--- makefiles/shared.mk
+++ makefiles/shared.mk
@@ -80,7 +80,7 @@ endif
 oas-page-build:
 	node ${OAS_MODULE_PATH} --bundle ${BUNDLE_PATH} --output ${REPO_DIR}/public --redoc ${REDOC_PATH} --repo ${REPO_DIR} --site-url ${URL}/${MUT_PREFIX}
 
-persist-data:
+persist-data: next-gen-parse
 	ifeq ($(USE_PERSISTENCE), true)
 		-node ${PERSISTENCE_MODULE_PATH} --path ${BUNDLE_PATH}
 	else
