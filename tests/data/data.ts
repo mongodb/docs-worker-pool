0f8fc242f021b520af3c066f5669012e31ffe95c
rayangler <27821750+rayangler@users.noreply.github.com>
2023-01-18 17:17:04 -0500

    DOP-3341: Integrate OAS Page Builder module as part of build process (#723)

--- tests/data/data.ts
+++ tests/data/data.ts
@@ -145,6 +145,7 @@ export class TestDataProvider {
     return Array<string>().concat(genericCommands.slice(0, genericCommands.length - 1), [
       'make get-build-dependencies',
       'make next-gen-html',
+      `make oas-page-build MUT_PREFIX=${job.payload.mutPrefix}`,
     ]);
   }
 
@@ -163,6 +164,8 @@ export class TestDataProvider {
     ]);
     if (job.payload.repoName == 'devhub-content-integration') {
       commands[commands.length - 1] += ` STRAPI_PUBLICATION_STATE=preview`;
+    } else {
+      commands.push(`make oas-page-build MUT_PREFIX=${job.payload.mutPrefix}`);
     }
     return commands;
   }
