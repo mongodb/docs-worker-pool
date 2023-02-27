d7d54412b8bb112749aded8ae95a2b2803bbe626
rayangler <27821750+rayangler@users.noreply.github.com>
2023-02-27 13:48:56 -0500

    Remove devhub dochub job

--- api/controllers/v1/dochub.ts
+++ api/controllers/v1/dochub.ts
@@ -1,10 +1,6 @@
 import { FastlyConnector } from '../../../src/services/cdn';
 import { ConsoleLogger } from '../../../src/services/logger';
 import { CDNCreds } from '../../../src/entities/creds';
-import * as c from 'config';
-import * as crypto from 'crypto';
-import * as mongodb from 'mongodb';
-import { JobRepository } from '../../../src/repositories/jobRepository';
 
 export const UpsertEdgeDictionaryItem = async (event: any = {}): Promise<any> => {
   const body = JSON.parse(event.body);
@@ -21,64 +17,4 @@ export const UpsertEdgeDictionaryItem = async (event: any = {}): Promise<any> =>
     headers: { 'Content-Type': 'text/plain' },
     body: 'success',
   };
-};
-
-async function prepDochubPushPayload() {
-  return {
-    title: 'DevHub CMS Prod Build',
-    user: 'jestapinski',
-    email: 'split@nothing.com',
-    status: 'inQueue',
-    createdTime: new Date(),
-    startTime: null,
-    endTime: null,
-    priority: 1,
-    error: {},
-    result: null,
-    payload: {
-      jobType: 'productionDeploy',
-      source: 'strapi',
-      action: 'push',
-      repoName: 'devhub-content',
-      branchName: 'master',
-      isFork: true,
-      private: true,
-      isXlarge: true,
-      repoOwner: '10gen',
-      url: 'https://github.com/10gen/devhub-content',
-      newHead: null,
-      urlSlug: null,
-      prefix: '',
-      project: 'devhub-content',
-    },
-    logs: [],
-  };
-}
-
-export const TriggerBuild = async (event: any = {}, context: any = {}): Promise<any> => {
-  const client = new mongodb.MongoClient(c.get('dbUrl'));
-  await client.connect();
-  const db = client.db(c.get('dbName'));
-  const consoleLogger = new ConsoleLogger();
-  const jobRepository = new JobRepository(db, c, consoleLogger);
-  // TODO: Make job be of type Job
-  const job = await prepDochubPushPayload();
-
-  try {
-    consoleLogger.info(job.title, 'Creating Job');
-    const jobId = await jobRepository.insertJob(job, c.get('jobsQueueUrl'));
-    consoleLogger.info(job.title, `Created Job ${jobId}`);
-    return {
-      statusCode: 202,
-      headers: { 'Content-Type': 'text/plain' },
-      body: jobId,
-    };
-  } catch (err) {
-    consoleLogger.error('TriggerBuild', err);
-    return {
-      statusCode: 500,
-      headers: { 'Content-Type': 'text/plain' },
-      body: err.message,
-    };
-  }
 };
