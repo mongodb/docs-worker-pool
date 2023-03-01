30fe1e3ff7cd04ba69606e9eed2da037e42bf880
branberry <brandon.ly@mongodb.com>
2023-03-01 14:31:13 -0600

    [DOP-3506]: refactor buildOpenAPIPages function

--- modules/oas-page-builder/src/services/pageBuilder.ts
+++ modules/oas-page-builder/src/services/pageBuilder.ts
@@ -15,9 +15,13 @@ const fetchTextData = async (url: string, errMsg: string) => {
   return res.text();
 };
 
-const getOASFileUrl = (gitHash: string) => `${OAS_FILE_SERVER}${gitHash}.json`;
+interface AtlasSpecUrlParams {
+  apiKeyword: string;
+  apiVersion?: string;
+  resourceVersion?: string;
+}
 
-const getAtlasSpecUrl = async (apiKeyword: string) => {
+const getAtlasSpecUrl = async ({ apiKeyword, apiVersion, resourceVersion }: AtlasSpecUrlParams) => {
   // Currently, the only expected API fetched programmatically is the Cloud Admin API,
   // but it's possible to have more in the future with varying processes.
   const keywords = ['cloud'];
@@ -25,11 +29,22 @@ const getAtlasSpecUrl = async (apiKeyword: string) => {
     throw new Error(`${apiKeyword} is not a supported API for building.`);
   }
 
+  let versionExtension = '';
+
+  if (apiVersion) {
+    versionExtension += `-${apiVersion}`;
+  }
+
+  if (apiVersion && resourceVersion) {
+    versionExtension += `-${resourceVersion}`;
+  }
+
   let oasFileURL;
+
   try {
     const versionURL = 'https://cloud.mongodb.com/version';
     const gitHash = await fetchTextData(versionURL, 'Could not find current version or git hash');
-    oasFileURL = getOASFileUrl(gitHash);
+    oasFileURL = `${OAS_FILE_SERVER}${gitHash}${versionExtension}.json`;
 
     // Sometimes the latest git hash might not have a fully available spec file yet.
     // If this is the case, we should default to using the last successfully saved
@@ -40,7 +55,7 @@ const getAtlasSpecUrl = async (apiKeyword: string) => {
 
     const res = await findLastSavedGitHash(apiKeyword);
     if (res) {
-      oasFileURL = getOASFileUrl(res.gitHash);
+      oasFileURL = `${OAS_FILE_SERVER}${res.gitHash}.json`;
       console.log(`Using ${oasFileURL}`);
     } else {
       throw new Error(`Could not find a saved hash for API: ${apiKeyword}`);
@@ -49,6 +64,41 @@ const getAtlasSpecUrl = async (apiKeyword: string) => {
 
   return oasFileURL;
 };
+
+interface GetOASpecParams {
+  sourceType: string;
+  source: string;
+  apiVersion?: string;
+  resourceVersion?: string;
+  redocExecutor: RedocExecutor;
+  output: string;
+  pageSlug: string;
+  repoPath: string;
+}
+async function getOASpec({ source, sourceType, repoPath, pageSlug, redocExecutor, output }: GetOASpecParams) {
+  try {
+    let spec = '';
+    const buildOptions: RedocBuildOptions = {};
+
+    if (sourceType === 'url') {
+      spec = source;
+    } else if (sourceType === 'local') {
+      const localFilePath = normalizePath(`${repoPath}/source/${source}`);
+      spec = localFilePath;
+    } else if (sourceType === 'atlas') {
+      spec = await getAtlasSpecUrl({ apiKeyword: source });
+      // Ignore "incompatible types" warnings for Atlas Admin API/cloud-docs
+      buildOptions['ignoreIncompatibleTypes'] = true;
+    } else {
+      throw new Error(`Unsupported source type "${sourceType}" for ${pageSlug}`);
+    }
+
+    const finalFilename = normalizePath(`${output}/${pageSlug}/index.html`);
+    await redocExecutor.execute(spec, finalFilename, buildOptions);
+  } catch (e) {
+    console.error(e);
+  }
+}
 
 export const buildOpenAPIPages = async (
   entries: [string, OASPageMetadata][],
@@ -59,30 +109,14 @@ export const buildOpenAPIPages = async (
   for (const [pageSlug, data] of entries) {
     const { source_type: sourceType, source, api_version: apiVersion, resource_versions: resourceVersions } = data;
 
-    try {
-      let spec = '';
-      const buildOptions: RedocBuildOptions = {};
-
-      if (sourceType === 'url') {
-        spec = source;
-      } else if (sourceType === 'local') {
-        const localFilePath = normalizePath(`${repoPath}/source/${source}`);
-        spec = localFilePath;
-      } else if (sourceType === 'atlas') {
-        spec = await getAtlasSpecUrl(source);
-        // Ignore "incompatible types" warnings for Atlas Admin API/cloud-docs
-        buildOptions['ignoreIncompatibleTypes'] = true;
-      } else {
-        throw new Error(`Unsupported source type "${sourceType}" for ${pageSlug}`);
+    if (apiVersion && resourceVersions) {
+      for (const resourceVersion in resourceVersions) {
+        getOASpec({ source, sourceType, output, pageSlug, redocExecutor, repoPath, apiVersion, resourceVersion });
       }
-
-      const finalFilename = normalizePath(`${output}/${pageSlug}/index.html`);
-      await redocExecutor.execute(spec, finalFilename, buildOptions);
-    } catch (e) {
-      console.error(e);
-      // Continue to try to build other pages since it's possible that mut will
-      // still upload existing HTML files
-      continue;
+    } else if (apiVersion && !resourceVersions) {
+      getOASpec({ source, sourceType, output, pageSlug, redocExecutor, repoPath, apiVersion });
+    } else {
+      getOASpec({ source, sourceType, output, pageSlug, redocExecutor, repoPath });
     }
   }
 };
