28ef394c167eba073e96b2b460802de11fa8873f
rayangler <27821750+rayangler@users.noreply.github.com>
2023-03-14 10:36:53 -0400

    DOP-3437: Remove DevHub code from Autobuilder (#772)

--- tests/data/data.ts
+++ tests/data/data.ts
@@ -162,11 +162,7 @@ export class TestDataProvider {
     const commands = Array<string>().concat(genericCommands.slice(0, genericCommands.length - 1), [
       'make next-gen-html',
     ]);
-    if (job.payload.repoName == 'devhub-content-integration') {
-      commands[commands.length - 1] += ` STRAPI_PUBLICATION_STATE=preview`;
-    } else {
-      commands.push(`make oas-page-build MUT_PREFIX=${job.payload.mutPrefix}`);
-    }
+    commands.push(`make oas-page-build MUT_PREFIX=${job.payload.mutPrefix}`);
     return commands;
   }
 
