3bcb55ebe9cc665196e2f25349eb3b7c175b5a3f
branberry <brandon.ly@mongodb.com>
2023-04-13 14:05:29 -0500

    [DOP-3637]: created simple dummy test

--- cdk-infra/test/cdk-infra.test.ts
+++ cdk-infra/test/cdk-infra.test.ts
@@ -1,16 +1,14 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as CdkInfra from '../lib/cdk-infra-stack';
+import * as cdk from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import { AutoBuilderStack } from '../lib/auto-builder-stack';
 
 // example test. To run these tests, uncomment this file along with the
 // example resource in lib/cdk-infra-stack.ts
-test('SQS Queue Created', () => {
-  //   const app = new cdk.App();
-  //     // WHEN
-  //   const stack = new CdkInfra.CdkInfraStack(app, 'MyTestStack');
-  //     // THEN
-  //   const template = Template.fromStack(stack);
-  //   template.hasResourceProperties('AWS::SQS::Queue', {
-  //     VisibilityTimeout: 300
-  //   });
+test('The stack is created with no issue', () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new AutoBuilderStack(app, 'MyTestStack');
+  // THEN
+  const template = Template.fromStack(stack);
+  template.resourceCountIs('AWS::SQS::Queue', 0);
 });
