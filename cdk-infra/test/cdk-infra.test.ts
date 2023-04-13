f6f6913d9c90ff4c7d9ebcf9450d99191b5099f2
branberry <brandon.ly@mongodb.com>
2023-04-13 13:41:47 -0500

    [DOP-3637]: Create AWS CDK project

---
+++ cdk-infra/test/cdk-infra.test.ts
@@ -0,0 +1,16 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkInfra from '../lib/cdk-infra-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-infra-stack.ts
+test('SQS Queue Created', () => {
+  //   const app = new cdk.App();
+  //     // WHEN
+  //   const stack = new CdkInfra.CdkInfraStack(app, 'MyTestStack');
+  //     // THEN
+  //   const template = Template.fromStack(stack);
+  //   template.hasResourceProperties('AWS::SQS::Queue', {
+  //     VisibilityTimeout: 300
+  //   });
+});
