8f062e6c8dda0cb1f6485d0c94a153962301032e
branberry <brandon.ly@mongodb.com>
2023-04-21 16:16:05 -0500

    [DOP-3639]: Create ts files to export specific lambda handler

--- cdk-infra/lib/constructs/webhooks-construct.ts
+++ cdk-infra/lib/constructs/webhooks-construct.ts
@@ -1,7 +1,7 @@
-import { LambdaRestApi } from 'aws-cdk-lib/aws-apigateway';
+import { LambdaIntegration, LambdaRestApi } from 'aws-cdk-lib/aws-apigateway';
 import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda';
 import { IQueue, Queue } from 'aws-cdk-lib/aws-sqs';
-import { Construct, Node } from 'constructs';
+import { Construct } from 'constructs';
 
 export class WebhookConstruct extends Construct {
   private readonly jobQueue: IQueue;
@@ -9,7 +9,11 @@ export class WebhookConstruct extends Construct {
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
-    const jobQueue = new Queue(this, 'JobQueue');
+    const slackLambda = new Function(this, 'slackLambda', {
+      code: Code.fromAsset('../build/controllers/v1/slack.zip'),
+      runtime: Runtime.NODEJS_14_X,
+      handler: 'slackLambda',
+    });
 
     const rootEndpointLambda = new Function(this, 'RootEndpointLambda', {
       code: Code.fromInline('exports.default = (event) => { console.log("hello, world!!"); }'),
@@ -20,6 +24,17 @@ export class WebhookConstruct extends Construct {
     const restApi = new LambdaRestApi(this, 'webhookHandlers', {
       handler: rootEndpointLambda,
     });
+
+    const controllersEndpoint = restApi.root.addResource('controllers');
+    const v1Endpoint = controllersEndpoint.addResource('v1');
+
+    const slackEndpoint = v1Endpoint.addResource('slack');
+    const dochubEndpoint = v1Endpoint.addResource('dochub');
+    const githubEndpoint = v1Endpoint.addResource('githubEndpoint');
+
+    slackEndpoint.addResource('trigger').addMethod('POST', new LambdaIntegration(slackLambda));
+
+    const jobQueue = new Queue(this, 'JobQueue');
 
     this.jobQueue = jobQueue;
   }
