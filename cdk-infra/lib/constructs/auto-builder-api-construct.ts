248a9cfeb711fa4299cc92bbd05a9449af4135f4
branberry <brandon.ly@mongodb.com>
2023-04-25 13:17:18 -0500

    [DOP-3639]: Delete zip script in favor of using NodejsFunction construct

--- cdk-infra/lib/constructs/auto-builder-api-construct.ts
+++ cdk-infra/lib/constructs/auto-builder-api-construct.ts
@@ -1,10 +1,12 @@
 import { Cors, CorsOptions, LambdaIntegration, LambdaRestApi } from 'aws-cdk-lib/aws-apigateway';
 import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { Queue } from 'aws-cdk-lib/aws-sqs';
 import { StringParameter } from 'aws-cdk-lib/aws-ssm';
 import { Construct } from 'constructs';
+import path = require('path');
 
-const HANDLERS_FILE_PATH = '../build/api/controllers/v1/handlers';
+const HANDLERS_FILE_PATH = '../api/controllers/v1';
 
 export class AutoBuilderApiConstruct extends Construct {
   constructor(scope: Construct, id: string) {
@@ -12,12 +14,10 @@ export class AutoBuilderApiConstruct extends Construct {
 
     const dbName = StringParameter.valueFromLookup(this, '/env/dev/docs/worker_pool/atlas/dbname');
 
-    const slackTriggerName = 'slackTriggerLambda';
-
-    const slackTriggerLambda = new Function(this, slackTriggerName, {
-      code: Code.fromAsset(`${HANDLERS_FILE_PATH}/slackTrigger.zip`),
+    const slackTriggerLambda = new NodejsFunction(this, 'slackTriggerLambda', {
+      entry: path.join(__dirname, `${HANDLERS_FILE_PATH}/slack.ts`),
       runtime: Runtime.NODEJS_14_X,
-      handler: slackTriggerName,
+      handler: 'DeployRepo',
       environment: {
         DB_NAME: dbName,
       },
@@ -33,12 +33,10 @@ export class AutoBuilderApiConstruct extends Construct {
       parameterName: '/env/dev/docs/worker_pool/fastly/dochub_map',
     }).stringValue;
 
-    const dochubTriggerName = 'dochubTriggerUpsertLambda';
-
-    const dochubTriggerUpsertLambda = new Function(this, dochubTriggerName, {
-      code: Code.fromAsset(`${HANDLERS_FILE_PATH}/dochubTriggerUpsert.zip`),
+    const dochubTriggerUpsertLambda = new NodejsFunction(this, 'dochubTriggerUpsertLambda', {
+      entry: path.join(__dirname, `${HANDLERS_FILE_PATH}/dochub.zip`),
       runtime: Runtime.NODEJS_14_X,
-      handler: dochubTriggerName,
+      handler: 'UpsertEdgeDictionaryItem',
       environment: {
         FASTLY_DOCHUB_MAP: fastlyDochubMap,
         FASTLY_DOCHUB_SERVICE_ID: fastlyDochubServiceId,
@@ -46,12 +44,16 @@ export class AutoBuilderApiConstruct extends Construct {
       },
     });
 
-    const githubTriggerName = 'githubTriggerLambda';
-
-    const githubTriggerLambda = new Function(this, githubTriggerName, {
-      code: Code.fromAsset(`${HANDLERS_FILE_PATH}/githubTriggerBuild.zip`),
+    const githubTriggerLambda = new NodejsFunction(this, 'githubTriggerLambda', {
+      entry: path.join(__dirname, `${HANDLERS_FILE_PATH}/github.ts`),
       runtime: Runtime.NODEJS_14_X,
-      handler: githubTriggerName,
+      handler: 'TriggerBuild',
+    });
+
+    const triggerLocalBuildLambda = new NodejsFunction(this, 'triggerLocalBuildLambda', {
+      entry: path.join(__dirname, `${HANDLERS_FILE_PATH}/jobs.ts`),
+      runtime: Runtime.NODEJS_14_X,
+      handler: 'TriggerLocalBuild',
     });
 
     // generic handler for the root endpoint
@@ -82,11 +84,15 @@ export class AutoBuilderApiConstruct extends Construct {
     dochubEndpoint.addResource('upsert').addMethod('POST', new LambdaIntegration(dochubTriggerUpsertLambda));
     githubEndpoint.addResource('trigger').addMethod('POST', new LambdaIntegration(githubTriggerLambda));
 
-    const jobQueue = new Queue(this, 'JobQueue');
+    const jobsQueue = new Queue(this, 'JobsQueue');
+    const jobUpdatesQueue = new Queue(this, 'JobUpdatesQueue');
 
-    // grant permission for lambdas to enqueue messages to the queue
-    jobQueue.grantSendMessages(slackTriggerLambda);
-    jobQueue.grantSendMessages(dochubTriggerUpsertLambda);
-    jobQueue.grantSendMessages(githubTriggerLambda);
+    // grant permission for lambdas to enqueue messages to the jobs queue
+    jobsQueue.grantSendMessages(slackTriggerLambda);
+    jobsQueue.grantSendMessages(githubTriggerLambda);
+    jobsQueue.grantSendMessages(triggerLocalBuildLambda);
+
+    // grant permission for lambds to enqueue messages to the job updates queue
+    jobUpdatesQueue.grantSendMessages(triggerLocalBuildLambda);
   }
 }
