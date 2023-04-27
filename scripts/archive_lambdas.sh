12752680a6d42ce0ab650c9bfb60641e0508ca63
branberry <brandon.ly@mongodb.com>
2023-04-27 13:56:49 -0500

    [DOP-3639]: Add updated script and add snapshot test

--- scripts/archive_lambdas.sh
+++ scripts/archive_lambdas.sh
@@ -4,6 +4,10 @@ parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
 cd ../
 
+# add additional assets for lambda code
 cp -a config/. build/config
 cp -a api/config/. build/api/config
+cp -a node_modules build
+
 zip -r build.zip build
+mv build.zip cdk_infra/
