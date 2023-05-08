8024ae54487014aef281df3ff5d25e8e580e15a0
rayangler <27821750+rayangler@users.noreply.github.com>
2023-05-08 17:18:13 -0400

    Add tests; move logic to pages service

--- modules/persistence/src/services/connector/index.ts
+++ modules/persistence/src/services/connector/index.ts
@@ -5,13 +5,10 @@
 
 import * as mongodb from 'mongodb';
 import { ObjectId, Db, Document } from 'mongodb';
-import isEqual from 'fast-deep-equal';
 import { db as poolDb } from './pool';
 
 // We should only ever have one client active at a time.
-// const atlasURL = `mongodb+srv://${process.env.MONGO_ATLAS_USERNAME}:${process.env.MONGO_ATLAS_PASSWORD}@${process.env.MONGO_ATLAS_HOST}/?retryWrites=true&w=majority&maxPoolSize=20`;
-// TODO-3394: Remove local connection
-const atlasURL = `mongodb://127.0.0.1:27017`;
+const atlasURL = `mongodb+srv://${process.env.MONGO_ATLAS_USERNAME}:${process.env.MONGO_ATLAS_PASSWORD}@${process.env.MONGO_ATLAS_HOST}/?retryWrites=true&w=majority&maxPoolSize=20`;
 const client = new mongodb.MongoClient(atlasURL);
 
 export const teardown = async () => {
@@ -53,7 +50,7 @@ export const insert = async (docs: any[], collection: string, buildId: ObjectId)
   }
 };
 
-const bulkWrite = async (operations: mongodb.AnyBulkWriteOperation[], collection: string) => {
+export const bulkWrite = async (operations: mongodb.AnyBulkWriteOperation[], collection: string) => {
   const dbSession = await db();
   try {
     return dbSession.collection(collection).bulkWrite(operations);
@@ -64,175 +61,19 @@ const bulkWrite = async (operations: mongodb.AnyBulkWriteOperation[], collection
 };
 
 // Upsert wrapper, requires an _id field.
-// TODO-3394: Use the new bulkWrite function?
-export const bulkUpsert = async (items: Document[], collection: string) => {
-  const upsertSession = await db();
-  try {
-    const operations: mongodb.AnyBulkWriteOperation[] = [];
-    items.forEach((item: Document) => {
-      const op = {
-        updateOne: {
-          filter: { _id: item._id },
-          update: { $set: item },
-          upsert: true,
-        },
-      };
-      operations.push(op);
-    });
-    return upsertSession.collection(collection).bulkWrite(operations);
-  } catch (error) {
-    console.error(`Error at bulk upsertion time for ${collection}: ${error}`);
-    throw error;
-  }
-};
-
-/**
- *
- * Finds the page documents for a given Snooty project name + branch combination.
- * If this is the first build for the Snooty project name + branch, no documents
- * will be found.
- *
- * @param pageIdPrefix - Includes the Snooty project name, user (docsworker-xlarge), and branch
- * @param collection - The collection to perform the find query on
- */
-const findPrevPageDocs = async (pageIdPrefix: string, collection: string) => {
-  const dbSession = await db();
-  const findQuery = {
-    page_id: { $regex: new RegExp(`^${pageIdPrefix}`) },
-    deleted: false,
-  };
-  const projection = {
-    _id: 0,
-    page_id: 1,
-    ast: 1,
-  };
-
-  try {
-    return dbSession.collection(collection).find(findQuery).project(projection);
-  } catch (error) {
-    console.error(
-      `Error trying to find previous page documents using prefix ${pageIdPrefix} in ${collection}}: ${error}`
-    );
-    throw error;
-  }
-};
-
-const createPageAstMapping = async (docsCursor: mongodb.FindCursor) => {
-  // Create mapping for page id and its AST
-  const mapping: Record<string, object> = {};
-  // Create set of all page ids. To be used for tracking unseen pages in the current build
-  const pageIds = new Set<string>();
-  for await (const doc of docsCursor) {
-    mapping[doc.page_id] = doc.ast;
-    pageIds.add(doc.page_id);
-  }
-  return { mapping, pageIds };
-};
-
-/**
- *
- * Compares the ASTs of the current pages with the previous pages. New update
- * operations are added whenever a diff in the page ASTs is found. Page IDs are
- * removed from `prevPageIds` to signal that the previous page has been "seen"
- *
- * @param operations - array of db operations. New operations will be pushed to this array when page diffs are found
- * @param updateTime - the time to set updates to
- * @param currentPages - the page documents for the current build
- * @param prevPageDocsMapping - a mapping of the page ASTs from the previous build
- * @param prevPageIds - a set of all page IDs from the previous build
- */
-const checkForPageDiffs = (
-  operations: mongodb.AnyBulkWriteOperation[],
-  updateTime: Date,
-  currentPages: Document[],
-  prevPageDocsMapping: Record<string, object>,
-  prevPageIds: Set<string>
-) => {
-  currentPages.forEach((page) => {
-    // Filter out rst (non-page) files
-    if (!page.filename.endsWith('.txt')) {
-      return;
-    }
-
-    const currentPageId = page.page_id;
-    prevPageIds.delete(currentPageId);
-
-    // Update the document if page's current AST is different from previous build's.
-    // New pages should always count as having a "different" AST
-    if (!isEqual(page.ast, prevPageDocsMapping[currentPageId])) {
-      const operation = {
-        updateOne: {
-          filter: { page_id: currentPageId },
-          update: {
-            $set: {
-              ...page,
-              updated_at: updateTime,
-              deleted: false,
-            },
-            $setOnInsert: {
-              created_at: updateTime,
-            },
-          },
-          upsert: true,
-        },
-      };
-      operations.push(operation);
-    }
-  });
-};
-
-/**
- *
- * Marks any pages from the previous build that were not used as "deleted"
- *
- * @param operations - array of db operations. New operations will be pushed to this array when pages are "deleted"
- * @param updateTime - the time to set updates to
- * @param unseenPageIds - a set of page IDs from the previous build that were not used for any comparisons
- */
-const markUnseenPagesAsDeleted = (
-  operations: mongodb.AnyBulkWriteOperation[],
-  updateTime: Date,
-  unusedPageIds: Set<string>
-) => {
-  unusedPageIds.forEach((unseenPageId) => {
-    const operation = {
+export const bulkUpsertAll = async (items: Document[], collection: string) => {
+  const operations: mongodb.AnyBulkWriteOperation[] = [];
+  items.forEach((item: Document) => {
+    const op = {
       updateOne: {
-        filter: { page_id: unseenPageId },
-        update: {
-          $set: {
-            deleted: true,
-            updated_at: updateTime,
-          },
-        },
+        filter: { _id: item._id },
+        update: { $set: item },
+        upsert: true,
       },
     };
-    operations.push(operation);
+    operations.push(op);
   });
-};
-
-export const bulkUpsertUpdatedPageDocuments = async (pages: Document[], collection: string) => {
-  console.time('updated_documents');
-  if (pages.length === 0) {
-    return;
-  }
-
-  // Find all pages that share the same project name + branch. Expects page IDs
-  // to include these two properties after parse
-  const pageIdPrefix = pages[0].page_id.split('/').slice(0, 3).join('/');
-  const previousPagesCursor = await findPrevPageDocs(pageIdPrefix, collection);
-  const { mapping: prevPageDocsMapping, pageIds: prevPageIds } = await createPageAstMapping(previousPagesCursor);
-
-  const operations: mongodb.AnyBulkWriteOperation[] = [];
-  const updateTime = new Date();
-
-  checkForPageDiffs(operations, updateTime, pages, prevPageDocsMapping, prevPageIds);
-  markUnseenPagesAsDeleted(operations, updateTime, prevPageIds);
-
-  if (operations.length > 0) {
-    const res = await bulkWrite(operations, collection);
-    console.log(res);
-  }
-  console.timeEnd('updated_documents');
+  return bulkWrite(operations, collection);
 };
 
 export const deleteDocuments = async (_ids: ObjectId[], collection: string) => {
--- modules/persistence/src/services/pages/index.ts
+++ modules/persistence/src/services/pages/index.ts
@@ -1,7 +1,28 @@
 import AdmZip from 'adm-zip';
 import { deserialize } from 'bson';
-import { ObjectId } from 'mongodb';
-import { bulkUpsertUpdatedPageDocuments, insert } from '../connector';
+import isEqual from 'fast-deep-equal';
+import { AnyBulkWriteOperation, Document, FindCursor, ObjectId } from 'mongodb';
+import { bulkWrite, db, insert } from '../connector';
+
+interface StaticAsset {
+  checksum: string;
+  key: string;
+}
+
+interface PageAst {
+  [key: string]: any;
+}
+
+export interface UpdatedPage {
+  page_id: string;
+  filename: string;
+  ast: PageAst;
+  static_assets: StaticAsset[];
+
+  created_at: Date;
+  updated_at: Date;
+  deleted: boolean;
+}
 
 const COLLECTION_NAME = 'documents';
 const UPDATED_AST_COLL_NAME = 'updated_documents';
@@ -16,14 +37,159 @@ const pagesFromZip = (zip: AdmZip) => {
     .map((entry) => deserialize(entry.getData()));
 };
 
-export const insertPages = async (buildId: ObjectId, zip: AdmZip) => {
+/**
+ *
+ * Finds the page documents for a given Snooty project name + branch combination.
+ * If this is the first build for the Snooty project name + branch, no documents
+ * will be found.
+ *
+ * @param pageIdPrefix - Includes the Snooty project name, user (docsworker-xlarge), and branch
+ * @param collection - The collection to perform the find query on
+ */
+const findPrevPageDocs = async (pageIdPrefix: string, collection: string) => {
+  const dbSession = await db();
+  const findQuery = {
+    page_id: { $regex: new RegExp(`^${pageIdPrefix}`) },
+    deleted: false,
+  };
+  const projection = {
+    _id: 0,
+    page_id: 1,
+    ast: 1,
+  };
+
+  try {
+    return dbSession.collection<UpdatedPage>(collection).find(findQuery).project(projection);
+  } catch (error) {
+    console.error(
+      `Error trying to find previous page documents using prefix ${pageIdPrefix} in ${collection}}: ${error}`
+    );
+    throw error;
+  }
+};
+
+const createPageAstMapping = async (docsCursor: FindCursor) => {
+  // Create mapping for page id and its AST
+  const mapping: Record<string, object> = {};
+  // Create set of all page ids. To be used for tracking unseen pages in the current build
+  const pageIds = new Set<string>();
+  for await (const doc of docsCursor) {
+    mapping[doc.page_id] = doc.ast;
+    pageIds.add(doc.page_id);
+  }
+  return { mapping, pageIds };
+};
+
+/**
+ *
+ * Compares the ASTs of the current pages with the previous pages. New update
+ * operations are added whenever a diff in the page ASTs is found. Page IDs are
+ * removed from `prevPageIds` to signal that the previous page has been "seen"
+ *
+ * @param operations - array of db operations. New operations will be pushed to this array when page diffs are found
+ * @param updateTime - the time to set updates to
+ * @param currentPages - the page documents for the current build
+ * @param prevPageDocsMapping - a mapping of the page ASTs from the previous build
+ * @param prevPageIds - a set of all page IDs from the previous build
+ */
+const checkForPageDiffs = (
+  operations: AnyBulkWriteOperation[],
+  updateTime: Date,
+  currentPages: Document[],
+  prevPageDocsMapping: Record<string, object>,
+  prevPageIds: Set<string>
+) => {
+  currentPages.forEach((page) => {
+    // Filter out rst (non-page) files
+    if (!page.filename.endsWith('.txt')) {
+      return;
+    }
+
+    const currentPageId = page.page_id;
+    prevPageIds.delete(currentPageId);
+
+    // Update the document if page's current AST is different from previous build's.
+    // New pages should always count as having a "different" AST
+    if (!isEqual(page.ast, prevPageDocsMapping[currentPageId])) {
+      const operation = {
+        updateOne: {
+          filter: { page_id: currentPageId },
+          update: {
+            $set: {
+              page_id: currentPageId,
+              filename: page.filename,
+              ast: page.ast,
+              static_assets: page.static_assets,
+              updated_at: updateTime,
+              deleted: false,
+            },
+            $setOnInsert: {
+              created_at: updateTime,
+            },
+          },
+          upsert: true,
+        },
+      };
+      operations.push(operation);
+    }
+  });
+};
+
+/**
+ *
+ * Marks any pages from the previous build that were not used as "deleted"
+ *
+ * @param operations - array of db operations. New operations will be pushed to this array when pages are "deleted"
+ * @param updateTime - the time to set updates to
+ * @param unseenPageIds - a set of page IDs from the previous build that were not used for any comparisons
+ */
+const markUnseenPagesAsDeleted = (
+  operations: AnyBulkWriteOperation[],
+  updateTime: Date,
+  unusedPageIds: Set<string>
+) => {
+  unusedPageIds.forEach((unseenPageId) => {
+    const operation = {
+      updateOne: {
+        filter: { page_id: unseenPageId },
+        update: {
+          $set: {
+            deleted: true,
+            updated_at: updateTime,
+          },
+        },
+      },
+    };
+    operations.push(operation);
+  });
+};
+
+export const updatePages = async (pages: Document[], collection: string) => {
+  if (pages.length === 0) {
+    return;
+  }
+
+  // Find all pages that share the same project name + branch. Expects page IDs
+  // to include these two properties after parse
+  const pageIdPrefix = pages[0].page_id.split('/').slice(0, 3).join('/');
+  const previousPagesCursor = await findPrevPageDocs(pageIdPrefix, collection);
+  const { mapping: prevPageDocsMapping, pageIds: prevPageIds } = await createPageAstMapping(previousPagesCursor);
+
+  const operations: AnyBulkWriteOperation[] = [];
+  const updateTime = new Date();
+
+  checkForPageDiffs(operations, updateTime, pages, prevPageDocsMapping, prevPageIds);
+  markUnseenPagesAsDeleted(operations, updateTime, prevPageIds);
+
+  if (operations.length > 0) {
+    await bulkWrite(operations, collection);
+  }
+};
+
+export const insertAndUpdatePages = async (buildId: ObjectId, zip: AdmZip) => {
   try {
     const pages = pagesFromZip(zip);
-    return Promise.all([
-      insert(pages, COLLECTION_NAME, buildId),
-      bulkUpsertUpdatedPageDocuments(pages, UPDATED_AST_COLL_NAME),
-    ]);
-    // return insert(pages, COLLECTION_NAME, buildId);
+    return Promise.all([insert(pages, COLLECTION_NAME, buildId), updatePages(pages, UPDATED_AST_COLL_NAME)]);
   } catch (error) {
     console.error(`Error at insertion time for ${COLLECTION_NAME}: ${error}`);
     throw error;
