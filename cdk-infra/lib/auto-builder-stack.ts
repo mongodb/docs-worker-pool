75049ea65fa5b29e4630933d4af9656dfd8574c2
branberry <brandon.ly@mongodb.com>
2023-05-22 09:31:14 -0500

    [DOP-3640]: Rename to simplify stuff

--- cdk-infra/lib/auto-builder-stack.ts
+++ cdk-infra/lib/auto-builder-stack.ts
@@ -1,8 +1,8 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { AutoBuilderApiConstruct } from './constructs/auto-builder-api-construct';
-import { AutoBuilderQueuesConstruct } from './constructs/auto-builder-queues-construct';
-import { AutoBuilderEnvConstruct } from './constructs/auto-builder-env-construct';
+import { AutoBuilderApiConstruct } from './constructs/api/api-construct';
+import { AutoBuilderQueuesConstruct } from './constructs/queue/queues-construct';
+import { AutoBuilderEnvConstruct } from './constructs/api/env-construct';
 
 export class AutoBuilderStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
---
+++ cdk-infra/lib/constructs/worker/worker-construct.ts
@@ -0,0 +1,7 @@
+import { Construct } from 'constructs';
+
+export class WorkerConstruct extends Construct {
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+  }
+}
