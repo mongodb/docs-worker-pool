51808e9e2e4009d95c196b6f17f7a86ca94e6b7c
branberry <brandon.ly@mongodb.com>
2023-05-23 12:50:04 -0500

    [DOP-3640]: Add task definition for fargate and create cluster

--- cdk-infra/lib/constructs/worker/worker-construct.ts
+++ cdk-infra/lib/constructs/worker/worker-construct.ts
@@ -1,10 +1,18 @@
-import { Bucket } from 'aws-cdk-lib/aws-s3';
+import { Vpc } from 'aws-cdk-lib/aws-ec2';
+import { Cluster, FargateTaskDefinition } from 'aws-cdk-lib/aws-ecs';
 import { Construct } from 'constructs';
 
 export class WorkerConstruct extends Construct {
+  readonly taskDefinitionArn: string;
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
-    const bucket = new Bucket(this, 'test');
+    const vpc = new Vpc(this, 'vpc');
+
+    new Cluster(this, 'cluster', { vpc, enableFargateCapacityProviders: true });
+
+    const taskDef = new FargateTaskDefinition(this, 'fargateTaskDef');
+
+    this.taskDefinitionArn = taskDef.taskDefinitionArn;
   }
 }
