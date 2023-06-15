626b9853c01738484c18e5bcbc5c2839bd05bb49
Cassidy Schaufele <cassidyschaufele@gmail.com>
2023-06-15 10:57:58 -0600

    [fix] Make linter happy

--- src/job/jobManager.ts
+++ src/job/jobManager.ts
@@ -116,7 +116,7 @@ export class JobManager {
     try {
       this._jobHandler = null;
       if (job?.payload) {
-        const excludeRepoFromBenchmarks = ['mms-docs','docs-k8s-operator'].includes(job.payload.repoName);
+        const excludeRepoFromBenchmarks = ['mms-docs', 'docs-k8s-operator'].includes(job.payload.repoName);
         // Can easily rollback with commenting out this flag.
         job.useWithBenchmark = !excludeRepoFromBenchmarks;
         await this.createHandlerAndExecute(job);
