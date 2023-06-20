d95b854de841446e0e2de3909f9c31a5cf176ea1
branberry <brandon.ly@mongodb.com>
2023-06-20 11:04:25 -0500

    [DOP-3726]: Resolve additional merge conflicts

--- cdk-infra/lib/constructs/api/webhook-env-construct.ts
+++ cdk-infra/lib/constructs/api/webhook-env-construct.ts
@@ -19,25 +19,16 @@ export class WebhookEnvConstruct extends Construct {
     const dbName = StringParameter.valueFromLookup(this, `${ssmPrefix}/atlas/dbname`);
     const dbUsername = StringParameter.valueFromLookup(this, `${ssmPrefix}/atlas/username`);
     const dbHost = StringParameter.valueFromLookup(this, `${ssmPrefix}/atlas/host`);
-<<<<<<< HEAD
     const jobCollection = StringParameter.valueFromLookup(this, `${ssmPrefix}/atlas/collections/job/queue`);
 
     const dbPassword = secureStrings['MONGO_ATLAS_PASSWORD'];
-=======
-
-    const dbPassword = secureStrings[`${ssmPrefix}/atlas/password`];
->>>>>>> master
     this.environment = {
       ...secureStrings,
       MONGO_ATLAS_USERNAME: dbUsername,
       MONGO_ATLAS_PASSWORD: dbPassword,
       MONGO_ATLAS_URL: `mongodb+srv://${dbUsername}:${dbPassword}@${dbHost}/admin?retryWrites=true`,
       DB_NAME: dbName,
-<<<<<<< HEAD
       JOB_QUEUE_COL_NAME: jobCollection,
-=======
-
->>>>>>> master
       NODE_CONFIG_DIR: './config',
       JOBS_QUEUE_URL: jobsQueue.queueUrl,
       JOB_UPDATES_QUEUE_URL: jobUpdatesQueue.queueUrl,
