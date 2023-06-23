7377307aae657de695e4b82e07f0fc618aa9a83d
Cassidy Schaufele <cassidyschaufele@gmail.com>
2023-06-23 08:11:10 -0600

    Await client.close invocation

--- api/controllers/v1/jobs.ts
+++ api/controllers/v1/jobs.ts
@@ -23,14 +23,14 @@ export const TriggerLocalBuild = async (event: any = {}, context: any = {}): Pro
     consoleLogger.info(body.jobId, 'enqueuing Job');
     await sqs.sendMessage(new JobQueueMessage(body.jobId, JobStatus.inQueue), c.get('jobUpdatesQueueUrl'), 0);
     consoleLogger.info(body.jobId, 'Job Queued Job');
-    client.close();
+    await client.close();
     return {
       statusCode: 202,
       headers: { 'Content-Type': 'text/plain' },
       body: body.jobId,
     };
   } catch (err) {
-    client.close();
+    await client.close();
     consoleLogger.error('TriggerLocalBuild', err);
     return {
       statusCode: 500,
@@ -108,9 +108,9 @@ export const FailStuckJobs = async () => {
   try {
     const hours = 8;
     await jobRepository.failStuckJobs(hours);
-    client.close();
+    await client.close();
   } catch (err) {
-    client.close();
+    await client.close();
     consoleLogger.error('FailStuckJobs', err);
   }
 };
@@ -128,9 +128,9 @@ async function saveTaskId(jobId: string, taskExecutionRes: any, consoleLogger: C
     // Only interested in the actual task ID since the whole ARN might have sensitive information
     const taskId = taskArn.split('/').pop();
     await jobRepository.addTaskIdToJob(jobId, taskId);
-    client.close();
+    await client.close();
   } catch (err) {
-    client.close();
+    await client.close();
     consoleLogger.error('saveTaskId', err);
   }
 }
@@ -187,7 +187,7 @@ async function NotifyBuildSummary(jobId: string): Promise<any> {
     ),
     entitlement['slack_user_id']
   );
-  client.close();
+  await client.close();
   return {
     statusCode: 200,
   };
@@ -291,7 +291,7 @@ async function NotifyBuildProgress(jobId: string): Promise<any> {
     ),
     entitlement['slack_user_id']
   );
-  client.close();
+  await client.close();
   return {
     statusCode: 200,
   };
@@ -341,5 +341,5 @@ async function SubmitArchiveJob(jobId: string) {
     repo.prefix[environment]
   );
   consoleLogger.info('submit archive job', JSON.stringify({ jobId: jobId, batchJobId: response.jobId }));
-  client.close();
+  await client.close();
 }
