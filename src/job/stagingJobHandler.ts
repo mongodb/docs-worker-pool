8822d589e26adc4be301221af13f9a6f49f1b4ad
Caesar Bell <caesar.jd.bell@gmail.com>
2023-07-13 16:48:39 -0400

    Update `stagingJobHandler.ts` (#854)
    * Update stagingJobHandler.ts
    
    This is a quick patch to see the boolean value appear in the log. It will be removed once we verify the build in Gatsby Cloud.
    
    * :rotating_light: Prettier lint fix
    
    ---------
    
    Co-authored-by: Caesar Bell <caesar.bell@mongodb.com>

--- src/job/stagingJobHandler.ts
+++ src/job/stagingJobHandler.ts
@@ -74,11 +74,16 @@ export class StagingJobHandler extends JobHandler {
       }
       // Invoke Gatsby Preview Webhook
       const featurePreviewWebhookEnabled = process.env.GATSBY_CLOUD_PREVIEW_WEBHOOK_ENABLED;
+      // Logging for Debugging purposes only will remove once we see the build working in Gatsby.
+      await this.logger.save(
+        this.currJob._id,
+        `${'(GATSBY_CLOUD_PREVIEW_WEBHOOK_ENABLED)'.padEnd(15)}${featurePreviewWebhookEnabled}`
+      );
       if (featurePreviewWebhookEnabled) {
         // TODO: current using a rudimentary logging approach, should switch to
         // something more robust once we are closer to going live.
         const response = await this.previewWebhook();
-        await this.logger.save(this.currJob._id, `${'(POST Webhook Status'.padEnd(15)}${response.status}`);
+        await this.logger.save(this.currJob._id, `${'(POST Webhook Status)'.padEnd(15)}${response.status}`);
       }
 
       await this.logger.save(this.currJob._id, `${'(stage)'.padEnd(15)}Finished pushing to staging`);
