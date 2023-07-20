1c85321d00a982a42d36c9598ada8116bfdae41b
rayangler <27821750+rayangler@users.noreply.github.com>
2023-07-20 17:05:20 -0400

    Use util functions for existing test

--- tests/unit/job/api/jobs.test.ts
+++ tests/unit/job/api/jobs.test.ts
@@ -1,9 +1,9 @@
-import crypto from 'crypto';
 import * as mongodb from 'mongodb';
-import { APIGatewayProxyEventHeaders } from 'aws-lambda';
 import { SnootyBuildComplete, extractUrlFromMessage } from '../../../../api/controllers/v1/jobs';
 import validURLFormatDoc from '../../../data/fullDoc';
 import invalidURLFormatDoc from '../../../data/fullDoc';
+import { createSha256Signature } from '../../../utils/createSha256Signature';
+import { createMockAPIGatewayEvent } from '../../../utils/createMockAPIGatewayEvent';
 
 jest.mock('config', () => ({
   get: () => 'SNOOTY_SECRET',
@@ -15,65 +15,6 @@ jest.mock('../../../../src/repositories/jobRepository', () => ({
     getJobById: jest.fn(),
   })),
 }));
-
-/**
- * Mocks the creation of a signature as if it was from Snooty
- * @param payloadString
- * @param secret
- */
-const createSnootySignature = (payloadString: string, secret: string) => {
-  return crypto.createHmac('sha256', secret).update(payloadString).digest('hex');
-};
-
-/**
- * Mocks the API Gateway Event object for API calls
- * @param payloadString - the payload for the event.body
- * @param headers - the headers to be included as part of the event
- * @returns a barebones API Gateway Event object
- */
-const createMockAPIGatewayEvent = (payloadString: string, headers: APIGatewayProxyEventHeaders = {}) => ({
-  body: payloadString,
-  headers,
-  multiValueHeaders: {},
-  httpMethod: 'POST',
-  isBase64Encoded: false,
-  path: '/foo',
-  pathParameters: {},
-  queryStringParameters: {},
-  multiValueQueryStringParameters: {},
-  stageVariables: {},
-  resource: '',
-  requestContext: {
-    accountId: '',
-    apiId: '',
-    authorizer: {},
-    protocol: 'HTTP/1.1',
-    httpMethod: 'POST',
-    identity: {
-      accessKey: '',
-      accountId: '',
-      apiKey: '',
-      apiKeyId: '',
-      caller: '',
-      clientCert: null,
-      cognitoAuthenticationProvider: '',
-      cognitoAuthenticationType: '',
-      cognitoIdentityId: '',
-      cognitoIdentityPoolId: '',
-      principalOrgId: '',
-      sourceIp: '',
-      user: '',
-      userAgent: '',
-      userArn: '',
-    },
-    path: '/foo',
-    stage: '',
-    requestId: '',
-    requestTimeEpoch: 0,
-    resourceId: '',
-    resourcePath: '',
-  },
-});
 
 describe('JobValidator Tests', () => {
   test('valid urls doc returns list of valid urls', () => {
@@ -105,7 +46,7 @@ describe('Post-build webhook tests', () => {
   });
 
   test('successfully completes job and performs notification', async () => {
-    const signature = createSnootySignature(payloadString, 'SNOOTY_SECRET');
+    const signature = createSha256Signature(payloadString, 'SNOOTY_SECRET');
     const res = await SnootyBuildComplete(
       createMockAPIGatewayEvent(payloadString, { 'x-snooty-signature': signature })
     );
@@ -113,7 +54,7 @@ describe('Post-build webhook tests', () => {
   });
 
   test('invalid signatures gracefully error', async () => {
-    const signature = createSnootySignature(payloadString, 'NOT_SNOOTY_SECRET');
+    const signature = createSha256Signature(payloadString, 'NOT_SNOOTY_SECRET');
     let res = await SnootyBuildComplete(createMockAPIGatewayEvent(payloadString, { 'x-snooty-signature': signature }));
     expect(res.statusCode).toBe(401);
 
@@ -125,7 +66,7 @@ describe('Post-build webhook tests', () => {
   test('invalid payloads gracefully error', async () => {
     const invalidPayload = { jobId: undefined };
     const payloadString = JSON.stringify(invalidPayload);
-    const signature = createSnootySignature(payloadString, 'SNOOTY_SECRET');
+    const signature = createSha256Signature(payloadString, 'SNOOTY_SECRET');
     const res = await SnootyBuildComplete(
       createMockAPIGatewayEvent(payloadString, { 'x-snooty-signature': signature })
     );
