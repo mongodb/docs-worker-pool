4ee84330e2b059b2da98dbfddf3895b67fee1742
Matt Meigs <matt.meigs@mongodb.com>
2023-07-28 15:59:49 -0400

    exit with 1, no undefined error

--- modules/persistence/index.ts
+++ modules/persistence/index.ts
@@ -58,8 +58,8 @@ const app = async (path: string, githubUser: string) => {
 console.log(argv);
 app(argv['path'], argv['githubUser']).catch((error) => {
   console.error('HEY WE ARE IN FINAL CATCH');
-  console.error('conditional result ', ['y', 'yes', 'true'].includes(argv['strict'].toLowerCase()));
-  console.error('argv strict ', argv['strict'].toLowerCase());
+  console.error('argv strict ', argv['strict']);
+  console.error('conditional result ', ['y', 'yes', 'true'].includes(argv['strict']));
   process.exit(1);
   // only exit with non zero error code if running with strict mode on
   if (['y', 'yes', 'true'].includes(argv['strict'].toLowerCase())) {
