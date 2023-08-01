5f6979b8db3d835e82907e11d4c30d26d63a1002
Matt Meigs <matt.meigs@mongodb.com>
2023-08-01 16:13:12 -0400

    last check: bubble

--- modules/persistence/src/services/metadata/index.ts
+++ modules/persistence/src/services/metadata/index.ts
@@ -69,7 +69,7 @@ export const insertMetadata = async (buildId: ObjectId, metadata: Metadata) => {
 
 export const insertMergedMetadataEntries = async (buildId: ObjectId, metadata: Metadata) => {
   try {
-    throw new Error();
+    throw new Error('Error messages still bubble up.');
     // const mergedMetadataEntries = await mergeAssociatedToCs(metadata);
     // return mergedMetadataEntries
     //   ? await Promise.all(mergedMetadataEntries.map((m) => insert([m], COLLECTION_NAME, buildId)))
