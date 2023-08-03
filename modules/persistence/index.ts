affe0a9b5808779d6a49e5e0c01fc48091054921
mmeigs <matt.meigs@mongodb.com>
2023-08-03 14:08:42 -0400

    DOP-3722: Allow errors to exit the Persistence Module (#868)
    * throw error in insertMergedMetadataEntries
    
    * point to DOP-3722-meta branch
    
    * add branch to deploy stg ecs workflow
    
    * also enhanced
    
    * log, for async reasons
    
    * chain catch in persistence
    
    * empty commit - build
    
    * exit 1 regardless
    
    * exit with 1, no undefined error
    
    * new meta
    
    * Empty-Commit
    
    * Empty-Commit2
    
    * Empty-Commit3
    
    * Empty-Commit4
    
    * clean log
    
    * log error and status from each command of next gen
    
    * clean, ensure
    
    * last check
    
    * last check: bubble
    
    * clean
    
    * remove from github action
    
    * revert to og meta branch

--- modules/persistence/index.ts
+++ modules/persistence/index.ts
@@ -55,15 +55,7 @@ const app = async (path: string, githubUser: string) => {
   }
 };
 
-try {
-  console.log(argv);
-  app(argv['path'], argv['githubUser']);
-} catch (error) {
-  console.log('caught in terminal handling');
-  // only exit with non zero error code if running with strict mode on
-  if (['y', 'yes', 'true'].includes(argv['strict'].toLowerCase())) {
-    process.exit(1);
-  } else {
-    process.exit(0);
-  }
-}
+app(argv['path'], argv['githubUser']).catch(() => {
+  console.error('Persistence Module Failure. Ending build.');
+  process.exit(1);
+});
