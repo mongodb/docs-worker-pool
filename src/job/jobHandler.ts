ac389132d575eeb8f0035db7d6330f974f611fd8
rayangler <27821750+rayangler@users.noreply.github.com>
2023-08-23 15:24:27 -0400

    Add missing feature flag for persistence module (#891)

--- src/job/jobHandler.ts
+++ src/job/jobHandler.ts
@@ -671,7 +671,7 @@ export abstract class JobHandler {
       if (!gatsbySiteId) {
         const message = `User ${githubUsername} does not have a Gatsby Cloud Site ID.`;
         this._logger.warn('Gatsby Cloud Preview Webhook', message);
-        throw new Error(message);
+        return;
       }
 
       const url = `${previewWebhookURL}/${gatsbySiteId}`;
