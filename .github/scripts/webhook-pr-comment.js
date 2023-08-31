16b5671d3b5b6574a196eb2fdb739554bafe3f6a
branberry <brandon.ly@mongodb.com>
2023-08-31 15:23:03 -0500

    [FEATURE]: Add try catch and conditional for commenting

--- .github/scripts/webhook-pr-comment.js
+++ .github/scripts/webhook-pr-comment.js
@@ -1,10 +1,16 @@
 const fs = require('fs');
 
 module.exports = ({ github }) => {
-  const outputsFile = fs.readFileSync('cdk-infra/outputs.json').toString();
-  const outputs = JSON.parse(outputsFile);
-  console.log(github.head_ref);
-  console.log(outputsFile);
-  const webhook = Object.values(outputs[`auto-builder-stack-enhancedApp-stg-${github.head_ref}-webhooks`])[0];
-  return webhook;
+  try {
+    const outputsFile = fs.readFileSync('cdk-infra/outputs.json').toString();
+    const outputs = JSON.parse(outputsFile);
+    console.log('github', github);
+    console.log(github.head_ref);
+    console.log(outputsFile);
+    const webhook = Object.values(outputs[`auto-builder-stack-enhancedApp-stg-${github.head_ref}-webhooks`])[0];
+    return webhook;
+  } catch (error) {
+    console.log('Error occurred when retrieving Webhook URL');
+    return '';
+  }
 };
