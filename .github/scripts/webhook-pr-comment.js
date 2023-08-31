0a2b540f56a70bec9941333fccab9b4a609afb56
branberry <brandon.ly@mongodb.com>
2023-08-31 15:21:00 -0500

    [FEATURE]: Log stuff for debugging :S

--- .github/scripts/webhook-pr-comment.js
+++ .github/scripts/webhook-pr-comment.js
@@ -3,6 +3,8 @@ const fs = require('fs');
 module.exports = ({ github }) => {
   const outputsFile = fs.readFileSync('cdk-infra/outputs.json').toString();
   const outputs = JSON.parse(outputsFile);
+  console.log(github.head_ref);
+  console.log(outputsFile);
   const webhook = Object.values(outputs[`auto-builder-stack-enhancedApp-stg-${github.head_ref}-webhooks`])[0];
   return webhook;
 };
