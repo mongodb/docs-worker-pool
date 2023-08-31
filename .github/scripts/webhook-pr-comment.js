a40ad340210f877bc95fe27d7677f4fc6a78aea9
branberry <brandon.ly@mongodb.com>
2023-08-31 14:32:50 -0500

    [FEATURE]: use correct cdk file path

--- .github/scripts/webhook-pr-comment.js
+++ .github/scripts/webhook-pr-comment.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 module.exports = ({ github }) => {
-  const outputsFile = fs.readFileSync('outputs.json').toString();
+  const outputsFile = fs.readFileSync('cdk-infra/outputs.json').toString();
   const outputs = JSON.parse(outputsFile);
   const webhook = Object.values(outputs[`auto-builder-stack-enhancedApp-stg-${github.head_ref}-webhooks`])[0];
   return webhook;
