428c6a0966c70d5b5643565092074092e7a76c4a
branberry <brandon.ly@mongodb.com>
2023-08-31 14:32:19 -0500

    [FEATURE]: Add commenting to PR

---
+++ .github/scripts/webhook-pr-comment.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+
+module.exports = ({ github }) => {
+  const outputsFile = fs.readFileSync('outputs.json').toString();
+  const outputs = JSON.parse(outputsFile);
+  const webhook = Object.values(outputs[`auto-builder-stack-enhancedApp-stg-${github.head_ref}-webhooks`])[0];
+  return webhook;
+};
