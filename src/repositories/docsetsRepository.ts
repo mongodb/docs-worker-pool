0cff1a1b37dc6fe6667039737ceb6bb70cac9998
Matt Meigs <matt.meigs@mongodb.com>
2023-09-19 12:03:26 -0400

    merge two separate docsetsRepos

--- src/repositories/docsetsRepository.ts
+++ src/repositories/docsetsRepository.ts
@@ -82,4 +82,37 @@ export class DocsetsRepository extends BaseRepository {
     }
     return { status: 'failure' };
   }
+
+  /**
+   * Compares the project path from a monorepo push event, and compares it with
+   * what is configured in the docset entry in Atlas.
+   * @param path The project path where the snooty.toml file exists from the monorepo.
+   * This path will reflect the current project path from a given commit.
+   * @param projectName The project name for the docset entry.
+   * @returns A boolean representing whether or not the configured docset entry snooty_toml path
+   * matches the path found in GitHub.
+   */
+  async checkSnootyTomlPath(path: string, projectName: string) {
+    const query = { project: projectName };
+    try {
+      const docsetObject = await this.findOne(
+        query,
+        `Mongo Timeout Error: Timedout while retrieving repos entry for ${path}`
+      );
+
+      if (!docsetObject) {
+        console.warn(`WARNING: The docset does not exist for the following project: ${projectName} \n path: ${path}`);
+
+        return false;
+      }
+
+      return docsetObject.directories.snooty_toml === path;
+    } catch (error) {
+      console.warn(
+        `WARNING: Error occurred when retrieving project path for ${projectName}. The following path was provided: ${path}`,
+        error
+      );
+      return false;
+    }
+  }
 }
