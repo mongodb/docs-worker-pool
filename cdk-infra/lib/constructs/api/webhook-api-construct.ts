5f8a3181d78ec7a1338c0b14d31a9d4854652876
branberry <brandon.ly@mongodb.com>
2023-10-16 12:19:24 -0500

    Add adot back in

--- cdk-infra/lib/constructs/api/webhook-api-construct.ts
+++ cdk-infra/lib/constructs/api/webhook-api-construct.ts
@@ -84,6 +84,10 @@ export class WebhookApiConstruct extends Construct {
       currentVersionOptions: {
         provisionedConcurrentExecutions: 5,
       },
+      adotInstrumentation: {
+        execWrapper: AdotLambdaExecWrapper.REGULAR_HANDLER,
+        layerVersion: AdotLayerVersion.fromJavaScriptSdkLayerVersion(AdotLambdaLayerJavaScriptSdkVersion.LATEST),
+      },
     });
 
     const githubDeleteArtifactsLambda = new NodejsFunction(this, 'githubDeleteArtifactsLambda', {
