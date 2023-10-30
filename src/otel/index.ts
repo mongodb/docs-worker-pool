4a70bfd945e6d03bc6a38457da432ab48bd2a0ff
branberry <brandon.ly@mongodb.com>
2023-10-30 16:35:33 -0500

    Use simple span processor

--- src/otel/index.ts
+++ src/otel/index.ts
@@ -4,7 +4,7 @@ import { AWSXRayPropagator } from '@opentelemetry/propagator-aws-xray';
 import { detectResourcesSync } from '@opentelemetry/resources';
 import { awsEcsDetector } from '@opentelemetry/resource-detector-aws';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-grpc';
-import { BatchSpanProcessor } from '@opentelemetry/sdk-trace-base';
+import { SimpleSpanProcessor } from '@opentelemetry/sdk-trace-base';
 import { HttpInstrumentation } from '@opentelemetry/instrumentation-http';
 import { AwsInstrumentation } from '@opentelemetry/instrumentation-aws-sdk';
 import { AWSXRayIdGenerator } from '@opentelemetry/id-generator-aws-xray';
@@ -15,7 +15,7 @@ export function nodeSDKBuilder() {
   });
 
   const traceExporter = new OTLPTraceExporter();
-  const spanProcessor = new BatchSpanProcessor(traceExporter);
+  const spanProcessor = new SimpleSpanProcessor(traceExporter);
 
   const sdk = new NodeSDK({
     textMapPropagator: new AWSXRayPropagator(),
