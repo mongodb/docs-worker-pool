c78f1c281a4e90ae78ab8c54ed78be417a36989a
branberry <brandon.ly@mongodb.com>
2023-11-22 09:20:56 -0600

    [DOP-4003]: Add meaningless commit to trigger webhook deploy

--- api/controllers/v2/github.ts
+++ api/controllers/v2/github.ts
@@ -59,7 +59,7 @@ async function prepGithubPushPayload(
 }
 
 export const TriggerBuild = async (event: APIGatewayEvent): Promise<APIGatewayProxyResult> => {
-  console.log('test');
+  console.log('testing');
   const client = new mongodb.MongoClient(c.get('dbUrl'));
   await client.connect();
   const db = client.db(c.get('dbName'));
