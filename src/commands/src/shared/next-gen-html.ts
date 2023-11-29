c0717cf31cab506ce37a63acecb2722b81e4900c
Matt Meigs <matt.meigs@mongodb.com>
2023-11-29 15:18:23 -0500

    log outputs

--- src/commands/src/shared/next-gen-html.ts
+++ src/commands/src/shared/next-gen-html.ts
@@ -4,24 +4,21 @@ import { executeCliCommand, getRepoDir } from '../helpers';
 import { Job } from '../../../entities/job';
 
 export async function nextGenHtml({ job, preppedLogger }: { job: Job; preppedLogger: (msg: string) => void }) {
-  // let cwd = path.join(`${process.cwd()}`, `../../../snooty`);
-  // if (repoName === 'docs-monorepo') {
-  //   cwd = path.join(`${process.cwd()}`, `../../snooty`);
-  // }
-
   const repoDir = getRepoDir(job.payload.repoName, job.payload.directory);
 
   preppedLogger(`Now running first cp command: cp ${repoDir}/.env.production ${repoDir}/snooty`);
-  await executeCliCommand({
+  const firstResult = await executeCliCommand({
     command: 'cp',
     args: [`${repoDir}/.env.production`, `${repoDir}/snooty`],
   });
+  preppedLogger(`Result of cp : ${firstResult.outputText}\n ${firstResult.errorText}`);
 
   preppedLogger(`Now cd into snooty`);
-  await executeCliCommand({
+  const secondResult = await executeCliCommand({
     command: 'cd',
     args: [`snooty`],
   });
+  preppedLogger(`Result of cd : ${secondResult.outputText}\n ${secondResult.errorText}`);
 
   preppedLogger(`nextGenHtml cwd: npm run build ${process.cwd()}/snooty`);
 
@@ -31,13 +28,14 @@ export async function nextGenHtml({ job, preppedLogger }: { job: Job; preppedLog
     options: { cwd: `${process.cwd()}/snooty` },
   });
 
-  preppedLogger(`Result of html: ${result}`);
+  preppedLogger(`Result of html: ${result.outputText}\n ${result.errorText}`);
 
   preppedLogger(`Now running second cp command: cp -r ${repoDir}/snooty/public ${repoDir}`);
-  await executeCliCommand({
+  const lastResult = await executeCliCommand({
     command: 'cp',
     args: [`-r`, `${repoDir}/snooty/public`, `${repoDir}`],
   });
+  preppedLogger(`Result of last cp : ${lastResult.outputText}\n ${lastResult.errorText}`);
 
   return result;
 }
