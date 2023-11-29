a3cbc729869f2708bdf47cc6e0444e4e16de1bac
Matt Meigs <matt.meigs@mongodb.com>
2023-11-29 16:02:28 -0500

    correct reposDir vs repoDir

--- src/commands/src/shared/next-gen-html.ts
+++ src/commands/src/shared/next-gen-html.ts
@@ -4,12 +4,13 @@ import { executeCliCommand, getRepoDir } from '../helpers';
 import { Job } from '../../../entities/job';
 
 export async function nextGenHtml({ job, preppedLogger }: { job: Job; preppedLogger: (msg: string) => void }) {
-  const repoDir = getRepoDir(job.payload.repoName, job.payload.directory);
+  // const repoDir = getRepoDir(job.payload.repoName, job.payload.directory);
+  const reposDir = path.join(process.cwd(), `/repos`);
 
-  preppedLogger(`Now running first cp command: cp ${repoDir}/.env.production ${repoDir}/snooty`);
+  preppedLogger(`Now running first cp command: cp ${reposDir}/.env.production ${reposDir}/snooty`);
   const firstResult = await executeCliCommand({
     command: 'cp',
-    args: [`${repoDir}/.env.production`, `${repoDir}/snooty`],
+    args: [`${reposDir}/.env.production`, `${reposDir}/snooty`],
     logger: preppedLogger,
   });
   preppedLogger(`Result of cp : ${firstResult.outputText}\n ${firstResult.errorText}`);
@@ -17,7 +18,7 @@ export async function nextGenHtml({ job, preppedLogger }: { job: Job; preppedLog
   preppedLogger(`Now cd into snooty`);
   const secondResult = await executeCliCommand({
     command: 'cd',
-    args: [`${repoDir}/snooty`],
+    args: [`${reposDir}/snooty`],
     logger: preppedLogger,
   });
   preppedLogger(`Result of cd : ${secondResult.outputText}\n ${secondResult.errorText}`);
@@ -27,16 +28,16 @@ export async function nextGenHtml({ job, preppedLogger }: { job: Job; preppedLog
   const result = await executeCliCommand({
     command: 'npm',
     args: ['run', 'build'],
-    options: { cwd: `${process.cwd()}/snooty` },
+    options: { cwd: `${reposDir}/snooty` },
     logger: preppedLogger,
   });
 
   preppedLogger(`Result of html: ${result.outputText}\n ${result.errorText}`);
 
-  preppedLogger(`Now running second cp command: cp -r ${repoDir}/snooty/public ${repoDir}`);
+  preppedLogger(`Now running second cp command: cp -r ${reposDir}/snooty/public ${reposDir}`);
   const lastResult = await executeCliCommand({
     command: 'cp',
-    args: [`-r`, `${repoDir}/snooty/public`, `${repoDir}`],
+    args: [`-r`, `${reposDir}/snooty/public`, `${reposDir}`],
     logger: preppedLogger,
   });
   preppedLogger(`Result of last cp : ${lastResult.outputText}\n ${lastResult.errorText}`);
