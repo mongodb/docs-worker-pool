1c063bb83b736b452495a3baf7e7e2cabedc666e
Brandon Ly <brandon.ly@mongodb.com>
2023-11-30 09:21:46 -0600

    [DOP-4097]: Ensure we use the --no-caching flag for production builds (#940)
    * [DOP-4097]: Add no caching flag for ts command
    
    * [DOP-4097]: Add NO_CACHING flag to next-gen-parse for production jobHandler
    
    * [DOP-4097]: Add NO_CACHING flag to next-gen-parse to staging for testing
    
    * [DOP-4097]: Add the right shared.mk path for right dockerfile
    
    * [DOP-4097]: Revert
    
    * [DOP-4097]: Fix dockerfile
    
    * [DOP-4097]: Update comments

--- src/commands/src/shared/next-gen-parse.ts
+++ src/commands/src/shared/next-gen-parse.ts
@@ -1,13 +1,18 @@
 import { CliCommandResponse, executeCliCommand } from '../helpers';
 
 const RSTSPEC_FLAG = '--rstspec=https://raw.githubusercontent.com/mongodb/snooty-parser/latest/snooty/rstspec.toml';
-
 interface NextGenParseParams {
   repoDir: string;
   commitHash: string;
   patchId?: string;
+  isProd?: boolean;
 }
-export async function nextGenParse({ repoDir, patchId, commitHash }: NextGenParseParams): Promise<CliCommandResponse> {
+export async function nextGenParse({
+  repoDir,
+  patchId,
+  commitHash,
+  isProd,
+}: NextGenParseParams): Promise<CliCommandResponse> {
   const commandArgs = ['build', repoDir, '--output', `${repoDir}/bundle.zip`, RSTSPEC_FLAG];
 
   if (patchId) {
@@ -16,6 +21,12 @@ export async function nextGenParse({ repoDir, patchId, commitHash }: NextGenPars
 
     commandArgs.push('--patch');
     commandArgs.push(patchId);
+  }
+
+  // Not currently used in production builds, adding functionality
+  // now so that it is available when it is.
+  if (isProd) {
+    commandArgs.push('--no-caching');
   }
 
   return executeCliCommand({ command: 'snooty', args: commandArgs });
