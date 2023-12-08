c7183b8b4b8ba841a608cbcc79f631bec5acd161
branberry <brandon.ly@mongodb.com>
2023-12-08 14:39:40 -0600

    [DOP-4170]: Create stack to deploy cache updater

---
+++ cdk-infra/lib/constructs/cache-updater/cache-updater-api-construct.ts
@@ -0,0 +1,18 @@
+import { DockerImageFunction, DockerImageCode } from 'aws-cdk-lib/aws-lambda';
+import { Construct } from 'constructs';
+import path from 'path';
+
+export class CacheUpdaterConstruct extends Construct {
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+
+    new DockerImageFunction(this, 'cacheUpdaterLambda', {
+      code: DockerImageCode.fromImageAsset(path.join(__dirname, '../../../../'), {
+        buildArgs: {
+          SNOOTY_PARSER_VERSION: '0.15.0',
+        },
+        file: 'api/handlers/cache-updater/Dockerfile',
+      }),
+    });
+  }
+}
