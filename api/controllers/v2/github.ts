2adf502faec63b841d0cd77751e2be134c730210
Matt Meigs <matt.meigs@mongodb.com>
2023-12-11 15:30:47 -0500

    add to v1?

--- api/controllers/v2/github.ts
+++ api/controllers/v2/github.ts
@@ -120,10 +120,6 @@ export const TriggerBuild = async (event: APIGatewayEvent): Promise<APIGatewayPr
     consoleLogger.info(job.title, `Created Job ${jobId}`);
   }
 
-  consoleLogger.info(body.repository.full_name, `IN V2!!! `);
-  consoleLogger.info(body.repository.full_name, `body repo name ${body.repository.name}`);
-  consoleLogger.info(body.repository.full_name, `feature flag value: ${process.env.FEATURE_FLAG_MONOREPO_PATH} `);
-
   if (body.repository.name === MONOREPO_NAME) {
     // TODO: MONOREPO feature flag needed here
     consoleLogger.info(body.repository.full_name, `past feature flag and monorepo conditional`);
