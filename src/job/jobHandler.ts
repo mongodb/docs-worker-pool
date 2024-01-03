9ef258205951ca7f125c2aa5fa1b5ca26d1242b4
Maya Raman <maya.raman19@gmail.com>
2024-01-03 13:04:09 -0500

    adding monorepo handling

--- src/job/jobHandler.ts
+++ src/job/jobHandler.ts
@@ -214,7 +214,9 @@ export abstract class JobHandler {
 
   @throwIfJobInterupted()
   private async getBuildDependencies() {
-    const buildDependencies = await this._repoBranchesRepo.getBuildDependencies(this.currJob.payload.repoName);
+    const repoName = this.currJob.payload.repoName;
+    const directory = this.currJob.payload.repoName === MONOREPO_NAME ? this.currJob.payload.directory : undefined;
+    const buildDependencies = await this._repoBranchesRepo.getBuildDependencies(repoName, directory);
     await this._logger.save(this._currJob._id, `logging buildDependencies: ${JSON.stringify(buildDependencies)}`);
     if (!buildDependencies) return [];
     return buildDependencies;
