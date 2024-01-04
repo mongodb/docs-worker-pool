46e0aa487fd8b37260bb38dec658debf37aaa07c
branberry <brandon.ly@mongodb.com>
2024-01-04 15:11:18 -0600

    [DOP-4170]: Add fargate task def and custer

--- cdk-infra/lib/constructs/cache-updater/cache-updater-worker-construct.ts
+++ cdk-infra/lib/constructs/cache-updater/cache-updater-worker-construct.ts
@@ -1,12 +1,37 @@
 import { IVpc } from 'aws-cdk-lib/aws-ec2';
+import { Cluster, ContainerImage, FargateTaskDefinition } from 'aws-cdk-lib/aws-ecs';
 import { Construct } from 'constructs';
+import path from 'path';
 
 interface CacheUpdaterWorkerConstructProps {
   vpc: IVpc;
 }
 
 export class CacheUpdaterWorkerConstruct extends Construct {
-  constructor(scope: Construct, id: string) {
+  clusterName: string;
+
+  constructor(scope: Construct, id: string, { vpc }: CacheUpdaterWorkerConstructProps) {
     super(scope, id);
+
+    // want to create fargate task def, do I even need cluster?
+    // I do, because this represents the task_definition_family value, and
+    // I need to start a task within a cluster.
+    const cluster = new Cluster(this, 'cacheUpdaterCluster', {
+      vpc,
+    });
+
+    const taskDefinition = new FargateTaskDefinition(this, 'cacheUpdaterWorker', {
+      cpu: 2048,
+      memoryLimitMiB: 4096,
+    });
+
+    taskDefinition.addContainer('cacheUpdaterWorkerImage', {
+      image: ContainerImage.fromAsset(path.join(__dirname, '../../../../api/handlers/cache-updater'), {
+        file: 'Dockerfile.cacheUpdater',
+        exclude: ['tests/', 'node_modules/', 'cdk-infra/'], // adding this just in case it doesn't pick up our dockerignore
+      }),
+    });
+
+    this.clusterName = cluster.clusterName;
   }
 }
