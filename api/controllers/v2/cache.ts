5fd0bd5b1e64e1a81e3571d64dc6dd7f3d7303e1
branberry <brandon.ly@mongodb.com>
2024-01-18 13:01:45 -0600

    [DOP-4171]: Add basic lambda handler

--- api/controllers/v2/cache.ts
+++ api/controllers/v2/cache.ts
@@ -1,6 +1,7 @@
 import { APIGatewayEvent, APIGatewayProxyResult } from 'aws-lambda';
 
 export async function rebuildCacheHandler(event: APIGatewayEvent): Promise<APIGatewayProxyResult> {
+  console.log(event.body);
   return {
     statusCode: 200,
     body: 'ok',
