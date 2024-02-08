88f205b038cfc0a1ba22cbd513e549feb78cfba9
Anabella Buckvar <anabellabuckvar@gmail.com>
2024-02-08 13:14:12 -0500

    DOP-4356 retrying sort accurately

--- src/services/slack.ts
+++ src/services/slack.ts
@@ -194,7 +194,14 @@ export class SlackConnector implements ISlackConnector {
     // THis is the limitation enforced by slack as no more 100 items are allowd in the dropdown
     //'[ERROR] no more than 100 items allowed [json-pointer:/view/blocks/0/element/options]'
 
-    reposToShow.sort().reverse();
+    reposToShow
+      .sort((a, b) =>
+        a.text
+          .toString()
+          .replace(/\d+/g, (n) => +n + 1000)
+          .localeCompare(b.text.toString().replace(/\d+/g, (n) => +n + 1000))
+      )
+      .reverse();
 
     if (reposToShow.length > 100) {
       reposToShow = reposToShow.splice(0, 100);
