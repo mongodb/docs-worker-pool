db27916ba1ec05e384a9ad02a1cbde3b28758bc3
Maya Raman <maya.raman19@gmail.com>
2024-02-12 15:52:29 -0500

    test app

---
+++ src/commands/src/helpers/axios-test.ts
@@ -0,0 +1,20 @@
+import axios from 'axios';
+import fs from 'fs';
+import { executeCliCommand, getCommitBranch, getCommitHash, getPatchId, getRepoDir } from '.';
+
+export async function testAxios(repoName: string, directory?: string) {
+  const repoDir = getRepoDir(repoName, directory);
+  const buildPath = `${repoDir}/source/driver-examples/maya.go`;
+  const url = 'https://raw.githubusercontent.com/mongodb/mongo-go-driver/master/internal/docexamples/examples.go';
+  try {
+    const res = await axios.get(url, { timeout: 10000, responseType: 'stream' });
+    console.log('MAYA RES');
+    console.log(res);
+    await res.data.pipe(fs.createWriteStream(buildPath));
+    console.log('PIPED? IDK');
+    await executeCliCommand({ command: 'cat', args: [`${buildPath}`] });
+    return `Downloading ${url} into ${buildPath}`;
+  } catch (error) {
+    return `ERROR! Could not download ${url} into ${buildPath}. ${error}`;
+  }
+}
