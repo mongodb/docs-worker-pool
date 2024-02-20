3403c8d7485e5bacedac5d2dccb0784bb63c9c40
branberry <brandon.ly@mongodb.com>
2024-02-20 10:23:06 -0600

    [DOP-4334]: Update comment pr action

--- api/controllers/v2/test-deploy.ts
+++ api/controllers/v2/test-deploy.ts
@@ -1,6 +1,8 @@
 import { APIGatewayEvent, APIGatewayProxyResult } from 'aws-lambda';
 
 export async function handleTestDeployRequest(event: APIGatewayEvent): Promise<APIGatewayProxyResult> {
+  const jobsQueueUrl = process.env.JOBS_QUEUE_URL;
+
   return {
     statusCode: 500,
     body: 'not complete',
