76d0a6c04239ba34694e9c42eb87f10d163ccc86
anabellabuckvar <41971124+anabellabuckvar@users.noreply.github.com>
2024-02-29 08:03:26 -0800

    DOP-4401 testing logger

--- src/job/jobHandler.ts
+++ src/job/jobHandler.ts
@@ -155,10 +155,10 @@ export abstract class JobHandler {
       this.currJob.payload.pathPrefix = pathPrefix;
       const mutPrefix = pathPrefix.split(`/${server_user}`)[0];
       if (this.currJob?.payload?.newHead && this.currJob?.title !== 'Merge to main') {
-        this._logger.save(this.currJob?.payload?.newHead, `Testing7 Commit Hash: ${this.currJob?.payload?.newHead}`);
+        this._logger.save(this.currJob._id, `Testing7 Commit Hash: ${this.currJob?.payload?.newHead}`);
       }
-      this._logger.save(mutPrefix.split('/')[0], `Testing7 Commit Hash: ${this.currJob?.payload?.newHead}`);
-      this._logger.save(mutPrefix.split('/')[0], 'MUT PREFIX9: ' + mutPrefix.split('/')[0]) + mutPrefix.split('/')[1];
+      this._logger.save(this.currJob._id, `Testing7 Commit Hash: ${this.currJob?.payload?.newHead}`);
+      this._logger.save(this.currJob._id, 'MUT PREFIX9: ' + mutPrefix.split('/')[0]) + mutPrefix.split('/')[1];
       this.currJob.payload.mutPrefix = mutPrefix;
     }
   }
