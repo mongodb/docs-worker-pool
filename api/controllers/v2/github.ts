c187a3697ab61f1172284d52ddbbcdc6e2feee62
anabellabuckvar <41971124+anabellabuckvar@users.noreply.github.com>
2024-03-12 19:27:44 -0400

    DOP-4414 testing lambda

--- api/controllers/v2/github.ts
+++ api/controllers/v2/github.ts
@@ -226,7 +226,7 @@ export const triggerSmokeTestAutomatedBuild = async (event: APIGatewayEvent): Pr
     return {
       statusCode: 202,
       headers: { 'Content-Type': 'text/plain' },
-      body: 'Jobs Queued 2' + body,
+      body: 'Jobs Queued 5' + body,
     };
   } catch (err) {
     return {
