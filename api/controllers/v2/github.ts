0dd6ecc66449dd377bb11b2f013af2069034a2de
anabellabuckvar <41971124+anabellabuckvar@users.noreply.github.com>
2024-03-15 11:32:51 -0400

    DOP-4414 remove validation

--- api/controllers/v2/github.ts
+++ api/controllers/v2/github.ts
@@ -95,7 +95,7 @@ async function createPayload(
     branchName,
     project,
     prefix,
-    urlSlug,
+    urlSlug: '',
     isFork,
     url,
     newHead,
@@ -134,14 +134,14 @@ export const triggerSmokeTestAutomatedBuild = async (event: APIGatewayEvent): Pr
   }
 
   // validate credentials here
-  if (!validateJsonWebhook(event, 'mongodbsmoketesting')) {
-    const errMsg = "X-Hub-Signature incorrect. Github webhook token doesn't match";
-    return {
-      statusCode: 401,
-      headers: { 'Content-Type': 'text/plain' },
-      body: errMsg,
-    };
-  }
+  // if (!validateJsonWebhook(event, 'mongodbsmoketesting')) {
+  //   const errMsg = "X-Hub-Signature incorrect. Github webhook token doesn't match";
+  //   return {
+  //     statusCode: 401,
+  //     headers: { 'Content-Type': 'text/plain' },
+  //     body: errMsg,
+  //   };
+  // }
 
   let body: WorkflowRunCompletedEvent;
   try {
