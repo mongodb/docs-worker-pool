2563973c905e6e86245b92599d7830605756c92c
anabellabuckvar <41971124+anabellabuckvar@users.noreply.github.com>
2024-03-25 10:32:04 -0400

    DOP-4414 readded name

--- src/app.ts
+++ src/app.ts
@@ -40,7 +40,7 @@ async function init(): Promise<void> {
   )}/?retryWrites=true&w=majority`;
   const client = new mongodb.MongoClient(atlasURL);
   await client.connect();
-  db = client.db(c.get(''));
+  db = client.db(c.get('dbName'));
   consoleLogger = new ConsoleLogger();
   fileSystemServices = new FileSystemServices();
   jobCommandExecutor = new JobSpecificCommandExecutor();
