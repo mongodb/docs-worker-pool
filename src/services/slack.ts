4c0fb9063cc9073a70db015eaeb71693d576a464
anabellabuckvar <41971124+anabellabuckvar@users.noreply.github.com>
2024-04-01 17:55:47 -0400

    DOP-4490-b testing radio button

--- src/services/slack.ts
+++ src/services/slack.ts
@@ -220,6 +220,41 @@ export class SlackConnector implements ISlackConnector {
             },
           },
           deployAll,
+          {
+            type: 'section',
+            block_id: 'block_testing_option',
+            text: {
+              type: 'plain_text',
+              text: 'Check out these rad radio buttons',
+            },
+            accessory: {
+              type: 'radio_buttons',
+              action_id: 'this_is_an_action_id',
+              initial_option: {
+                value: 'A1',
+                text: {
+                  type: 'plain_text',
+                  text: 'Radio 1',
+                },
+              },
+              options: [
+                {
+                  value: 'A1',
+                  text: {
+                    type: 'plain_text',
+                    text: 'Radio 1',
+                  },
+                },
+                {
+                  value: 'A2',
+                  text: {
+                    type: 'plain_text',
+                    text: 'Radio 2',
+                  },
+                },
+              ],
+            },
+          },
         ],
       },
     };
