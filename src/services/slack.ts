ffca8fdc819e02ca9e4f50f0921c16f9c4d7a2c5
anabellabuckvar <41971124+anabellabuckvar@users.noreply.github.com>
2024-04-01 17:33:42 -0400

    DOP-4490-b changing button block type to actions

--- src/services/slack.ts
+++ src/services/slack.ts
@@ -136,7 +136,7 @@ export class SlackConnector implements ISlackConnector {
   private _getDropDownView(triggerId: string, repos: Array<any>, admin: boolean) {
     const deployAll = admin
       ? {
-          type: 'action',
+          type: 'actions',
           block_id: 'deploy_all_button',
           elements: [
             {
