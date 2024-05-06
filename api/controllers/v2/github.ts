e532d4124202c01483320d55d3703566defa7f60
anabellabuckvar <41971124+anabellabuckvar@users.noreply.github.com>
2024-05-06 11:33:01 -0400

    DOP-4451 pushing to preprd

--- api/controllers/v2/github.ts
+++ api/controllers/v2/github.ts
@@ -168,7 +168,7 @@ export const triggerSmokeTestAutomatedBuild = async (event: APIGatewayEvent): Pr
       body: `Workflow ${body.workflow_run.name} completed successfully but only Deploy Staging ECS workflow completion will trigger smoke test site deployments`,
     };
 
-  // if the build was not building main branch, no need for smoke test sites
+  // if the build was not building the main branch, no need for smoke test sites
 
   // if (body.workflow_run.head_branch != 'main' || body.repository.fork) {
   //   console.log('Build was not on master branch in main repo, sites will not deploy as no smoke tests are needed');
