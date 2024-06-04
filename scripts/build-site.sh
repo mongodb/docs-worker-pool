c6830b4140d2d399b922947c545c26054724165b
branberry <brandon.ly@mongodb.com>
2024-06-04 09:14:47 -0500

    Separate scripts

--- scripts/build-site.sh
+++ scripts/build-site.sh
@@ -1,6 +1,3 @@
-curl -L -o snooty-parser.zip https://github.com/mongodb/snooty-parser/releases/download/v0.16.6/snooty-v0.16.6-linux_x86_64.zip
-unzip -d ./snooty-parser snooty-parser.zip
-
 ls -l ./snooty-parser
 chmod +x ./snooty-parser/snooty
 ./snooty-parser/snooty/snooty build . --output=./bundle.zip
