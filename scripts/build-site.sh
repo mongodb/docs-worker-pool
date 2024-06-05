e569fb885bb4756fc39bdc85a5074a6f0ecfcc06
branberry <brandon.ly@mongodb.com>
2024-06-05 10:07:35 -0500

    Undo changes that I added back in on accident

--- scripts/build-site.sh
+++ scripts/build-site.sh
@@ -1,19 +1,12 @@
 ls -l ./snooty-parser
 chmod +x ./snooty-parser/snooty
 ./snooty-parser/snooty/snooty build . --output=./bundle.zip
-
-if [ ! -d "snooty" ]; then
-  echo "snooty not cloned, downloading"
-  
-  git clone -b v0.16.14 --depth 1 https://github.com/mongodb/snooty.git 
-  cd snooty
-  npm ci --legacy-peer-deps
-  git clone --depth 1 https://github.com/mongodb/docs-tools.git ./snooty/docs-tools
-  mkdir -p ./snooty/static/images
-  mv ./snooty/docs-tools/themes/mongodb/static ./static/docs-tools
-  mv ./snooty/docs-tools/themes/guides/static/images/bg-accent.svg ./static/docs-tools/images/bg-accent.svg
-fi
-
+git clone -b v0.16.14 --depth 1 https://github.com/mongodb/snooty.git 
 echo GATSBY_MANIFEST_PATH=$(pwd)/bundle.zip >> ./snooty/.env.production
-
+cd snooty                
+npm ci --legacy-peer-deps
+git clone --depth 1 https://github.com/mongodb/docs-tools.git ./snooty/docs-tools
+mkdir -p ./snooty/static/images
+mv ./snooty/docs-tools/themes/mongodb/static ./static/docs-tools
+mv ./snooty/docs-tools/themes/guides/static/images/bg-accent.svg ./static/docs-tools/images/bg-accent.svg
 cd snooty && npm run build
