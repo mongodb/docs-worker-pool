55fa7d3ca50c72e8e751fe9c0fb09be6b3cf4934
anabellabuckvar <41971124+anabellabuckvar@users.noreply.github.com>
2024-06-05 17:01:40 -0400

    Update build-site.sh

--- scripts/build-site.sh
+++ scripts/build-site.sh
@@ -1,19 +1,15 @@
 ls -l ./snooty-parser
 chmod +x ./snooty-parser/snooty
-./snooty-parser/snooty/snooty create-cache .
-
-if [ ! -d "snooty" ]; then
-  echo "snooty not cloned, downloading"
-  git clone -b netlify-poc --depth 1 https://github.com/mongodb/snooty.git 
-  echo GATSBY_MANIFEST_PATH=$(pwd)/bundle.zip >> ./snooty/.env.production
-  cd snooty
-  npm ci --legacy-peer-deps
-  git clone --depth 1 https://github.com/mongodb/docs-tools.git ./snooty/docs-tools
-  mkdir -p ./snooty/static/images
-  mv ./snooty/docs-tools/themes/mongodb/static ./static/docs-tools
-  mv ./snooty/docs-tools/themes/guides/static/images/bg-accent.svg ./static/docs-tools/images/bg-accent.svg
-fi
-
+./snooty-parser/snooty/snooty create-cache . --output=bundle.zip
+echo "snooty not cloned, downloading"
+git clone -b netlify-poc --depth 1 https://github.com/mongodb/snooty.git 
+echo GATSBY_MANIFEST_PATH=$(pwd)/bundle.zip >> ./snooty/.env.production
+cd snooty
+npm ci --legacy-peer-deps
+git clone --depth 1 https://github.com/mongodb/docs-tools.git ./snooty/docs-tools
+mkdir -p ./snooty/static/images
+mv ./snooty/docs-tools/themes/mongodb/static ./static/docs-tools
+mv ./snooty/docs-tools/themes/guides/static/images/bg-accent.svg ./static/docs-tools/images/bg-accent.svg
 
 
 cd snooty && npm run build
