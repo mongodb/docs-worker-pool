5d2572f98cc1be9cf5362b3886c21c8d52d7aed5
anabellabuckvar <41971124+anabellabuckvar@users.noreply.github.com>
2024-06-05 17:11:26 -0400

    Revert to OG version

--- scripts/build-site.sh
+++ scripts/build-site.sh
@@ -1,17 +1,12 @@
 ls -l ./snooty-parser
 chmod +x ./snooty-parser/snooty
-./snooty-parser/snooty/snooty create-cache .
 ./snooty-parser/snooty/snooty build . --output=./bundle.zip
-
-echo "snooty not cloned, downloading"
 git clone -b netlify-poc --depth 1 https://github.com/mongodb/snooty.git 
 echo GATSBY_MANIFEST_PATH=$(pwd)/bundle.zip >> ./snooty/.env.production
-cd snooty
+cd snooty                
 npm ci --legacy-peer-deps
 git clone --depth 1 https://github.com/mongodb/docs-tools.git ./snooty/docs-tools
 mkdir -p ./snooty/static/images
 mv ./snooty/docs-tools/themes/mongodb/static ./static/docs-tools
 mv ./snooty/docs-tools/themes/guides/static/images/bg-accent.svg ./static/docs-tools/images/bg-accent.svg
-
-
 cd snooty && npm run build
