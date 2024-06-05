2eaaf0c9eeaa9a4d44f0ec63cc0b80ec007d36b2
anabellabuckvar <41971124+anabellabuckvar@users.noreply.github.com>
2024-06-05 17:18:27 -0400

    Add in cache step

--- scripts/build-site.sh
+++ scripts/build-site.sh
@@ -1,6 +1,8 @@
 ls -l ./snooty-parser
 chmod +x ./snooty-parser/snooty
 ./snooty-parser/snooty/snooty build . --output=./bundle.zip
+./snooty-parser/snooty/snooty create-cache .
+
 git clone -b netlify-poc --depth 1 https://github.com/mongodb/snooty.git 
 echo GATSBY_MANIFEST_PATH=$(pwd)/bundle.zip >> ./snooty/.env.production
 cd snooty                
