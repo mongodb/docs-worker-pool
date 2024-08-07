c4fd780eb48b43f0187d942fe45f42277ef0cc9f
anabellabuckvar <41971124+anabellabuckvar@users.noreply.github.com>
2024-08-07 14:01:35 -0400

    Update build-site.sh

--- scripts/build-site.sh
+++ scripts/build-site.sh
@@ -1,4 +1,4 @@
-PARSER_VERSION=0.16.6
+PARSER_VERSION=0.16.15
 
 # This make command curls the examples for certain repos.
 # If the rule doesn't exist, the error doesn't interrupt the build process.
