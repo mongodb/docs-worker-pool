9a0e17e319cec6a5cdc113d1c63100412928f4e8
rayangler <27821750+rayangler@users.noreply.github.com>
2024-11-19 15:33:26 -0500

    Update

--- scripts/build-site.sh
+++ scripts/build-site.sh
@@ -1,4 +1,4 @@
-PARSER_VERSION=DOP-5189-test-5
+PARSER_VERSION=DOP-5189-test-7
 
 # This make command curls the examples for certain repos.
 # If the rule doesn't exist, the error doesn't interrupt the build process.
