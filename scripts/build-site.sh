766adf75dcdc39cac5b9686503c0f5fffd8b88b0
rayangler <27821750+rayangler@users.noreply.github.com>
2024-11-19 14:21:38 -0500

    Use test branch

--- scripts/build-site.sh
+++ scripts/build-site.sh
@@ -1,4 +1,4 @@
-PARSER_VERSION=0.18.6
+PARSER_VERSION=0.18.7
 
 # This make command curls the examples for certain repos.
 # If the rule doesn't exist, the error doesn't interrupt the build process.
@@ -6,7 +6,7 @@ make examples
 
 if [ ! -d "snooty-parser" ]; then
   echo "snooty parser not installed, downloading..."
-  curl -L -o snooty-parser.zip https://github.com/mongodb/snooty-parser/releases/download/v${PARSER_VERSION}/snooty-v${PARSER_VERSION}-linux_x86_64.zip
+  curl -L -o snooty-parser.zip https://github.com/rayangler/snooty-parser/releases/download/DOP-5189-test-2/snooty-DOP-5189-test-2-linux_x86_64.zip
   unzip -d ./snooty-parser snooty-parser.zip
   chmod +x ./snooty-parser/snooty
 fi
