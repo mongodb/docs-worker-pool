9490dde6fb7233e549dce97b2438f708f7ae5f87
Brandon Ly <brandon.ly@mongodb.com>
2025-02-13 15:37:20 -0600

    Update build-site.sh

--- scripts/build-site.sh
+++ scripts/build-site.sh
@@ -1,4 +1,4 @@
-PARSER_VERSION=0.18.15
+PARSER_VERSION=0.18.14
 
 # This make command curls the examples for certain repos.
 # If the rule doesn't exist, the error doesn't interrupt the build process.
