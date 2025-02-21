0679ebf5c23d9fd78760997c024cfc0eb1a49102
anabellabuckvar <41971124+anabellabuckvar@users.noreply.github.com>
2025-02-20 19:15:32 -0500

    Update build-site.sh to not use netlify-poc for frontend

--- scripts/build-site.sh
+++ scripts/build-site.sh
@@ -19,7 +19,7 @@ echo "==========================================================================
 
 if [ ! -d "snooty" ]; then
   echo "Snooty frontend not installed, downloading"
-  git clone -b netlify-poc --depth 1 https://github.com/mongodb/snooty.git 
+  git clone -b --depth 1 https://github.com/mongodb/snooty.git 
   echo GATSBY_MANIFEST_PATH=$(pwd)/bundle.zip >> ./snooty/.env.production
   cd snooty
   npm ci --legacy-peer-deps
